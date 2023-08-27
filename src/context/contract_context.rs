e5d4d0c8dc8276feef29700ec54166fec97a1ad9
Alex Roan <alex.roan@hotmail.com>
2023-08-27 23:20:28 +0100

    Basic pragma collector

--- src/context/contract_context.rs
+++ src/context/contract_context.rs
@@ -2,6 +2,36 @@ use eyre::Result;
 
 use solc_ast::{ast::*, visitor::ast_visitor::*};
 
-struct ContractContext {
+#[derive(Default, Debug)]
+pub struct ContractContext {
+    pub pragma: PragmaDirective
+}
 
+impl ASTConstVisitor for ContractContext {
+    fn end_visit_pragma_directive(&mut self, node: &PragmaDirective) -> Result<()> {
+        self.pragma = node.clone();
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod contract_context_tests {
+    use eyre::Result;
+    use solc_ast::{ast::*, visitor::ast_visitor::*};
+    use crate::context::contract_context::ContractContext;
+
+    fn read_abi_encode_packed() -> Result<SourceUnit> {
+        Ok(serde_json::from_reader(std::io::BufReader::new(
+            std::fs::File::open("tests/ast-json/AbiEncodePacked.json")?,
+        ))?)
+    }
+
+    #[test]
+    fn pragma_directives() -> Result<()> {
+        let source_unit = read_abi_encode_packed()?;
+        let mut context = ContractContext::default();
+        source_unit.accept(&mut context)?;
+        println!("{:?}", context.pragma);
+        Ok(())
+    }
 }
