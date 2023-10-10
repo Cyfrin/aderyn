7771869d93586936f2dbefd26e2befd3f397acf7
Alex Roan <alex.roan@hotmail.com>
2023-10-10 16:26:14 +0100

    Detector details

--- src/detector/detector.rs
+++ src/detector/detector.rs
@@ -1,8 +1,32 @@
 use crate::loader::loader::ContractLoader;
 use std::error::Error;
 
-pub trait Detector {
-    fn detect(&mut self, loader: &ContractLoader) -> Result<(), Box<dyn Error>>{
+pub enum IssueSeverity {
+    Gas,
+    Low,
+    Medium,
+    High,
+    Critical,
+}
+
+pub trait Detector<T> {
+    fn detect(&mut self, _loader: &ContractLoader) -> Result<(), Box<dyn Error>>{
         Ok(())
+    }
+
+    fn severity(&self) -> IssueSeverity {
+        IssueSeverity::Medium
+    }
+
+    fn title(&self) -> String {
+        String::from("Title")
+    }
+
+    fn description(&self) -> String {
+        String::from("Description")
+    }
+
+    fn get_instances(&self) -> Vec<T> {
+        Vec::new()
     }
 }
