2940dba221833f181cfa6864f7a83fbeecab0389
Alex Roan <alex.roan@hotmail.com>
2023-11-08 18:27:18 +0000

    lint

--- src/context/complexity.rs
+++ src/context/complexity.rs
@@ -1,12 +1,9 @@
 use eyre::Result;
-use std::{default, error::Error};
 
 use crate::{
     ast::*,
     visitor::ast_visitor::{ASTConstVisitor, Node},
 };
-
-use super::ast_node::ASTNode;
 
 #[derive(Default, Debug)]
 pub struct ComplexityLoader {
@@ -14,7 +11,7 @@ pub struct ComplexityLoader {
 }
 
 impl ComplexityLoader {
-    pub fn visit(&mut self, node: &SourceUnit) -> Result<(i32)> {
+    pub fn visit(&mut self, node: &SourceUnit) -> Result<i32> {
         self.complexity = 0;
 
         node.accept(self)?;
@@ -24,17 +21,17 @@ impl ComplexityLoader {
 }
 
 impl ASTConstVisitor for ComplexityLoader {
-    fn visit_if_statement(&mut self, node: &IfStatement) -> Result<bool> {
+    fn visit_if_statement(&mut self, _node: &IfStatement) -> Result<bool> {
         self.complexity += 1;
         Ok(true)
     }
 
-    fn visit_modifier_invocation(&mut self, node: &ModifierInvocation) -> Result<bool> {
+    fn visit_modifier_invocation(&mut self, _node: &ModifierInvocation) -> Result<bool> {
         self.complexity += 1;
         Ok(true)
     }
 
-    fn visit_function_call(&mut self, node: &FunctionCall) -> Result<bool> {
+    fn visit_function_call(&mut self, _node: &FunctionCall) -> Result<bool> {
         self.complexity += 1;
         Ok(true)
     }
@@ -52,34 +49,34 @@ impl ASTConstVisitor for ComplexityLoader {
         Ok(true)
     }
 
-    fn visit_new_expression(&mut self, node: &NewExpression) -> Result<bool> {
+    fn visit_new_expression(&mut self, _node: &NewExpression) -> Result<bool> {
         self.complexity += 10;
         Ok(true)
     }
 
-    fn visit_for_statement(&mut self, node: &ForStatement) -> Result<bool> {
+    fn visit_for_statement(&mut self, _node: &ForStatement) -> Result<bool> {
         self.complexity += 5;
         Ok(true)
     }
 
-    fn visit_while_statement(&mut self, node: &WhileStatement) -> Result<bool> {
+    fn visit_while_statement(&mut self, _node: &WhileStatement) -> Result<bool> {
         self.complexity += 1;
         Ok(true)
     }
 
-    fn visit_inline_assembly(&mut self, node: &InlineAssembly) -> Result<bool> {
+    fn visit_inline_assembly(&mut self, _node: &InlineAssembly) -> Result<bool> {
         self.complexity += 2;
         Ok(true)
     }
 
-    fn visit_conditional(&mut self, node: &Conditional) -> Result<bool> {
+    fn visit_conditional(&mut self, _node: &Conditional) -> Result<bool> {
         self.complexity += 1;
         Ok(true)
     }
 
     fn visit_contract_definition(&mut self, node: &ContractDefinition) -> Result<bool> {
         let mut comp = 1;
-        for base_contract in Some(&node.base_contracts) {
+        while let Some(_base_contract) = Some(&node.base_contracts) {
             comp += 2;
         }
         self.complexity += comp;
