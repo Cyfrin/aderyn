cc5e3a68f2378a387a4c85d91f6d8f663cb4e0c8
Alex Roan <alex@cyfrin.io>
2024-01-10 17:33:58 +0000

    NewExpression is exactly the same structure as ElementaryTypeNameExpression

--- aderyn_core/src/ast/expressions.rs
+++ aderyn_core/src/ast/expressions.rs
@@ -21,6 +21,8 @@ pub enum Expression {
     MemberAccess(MemberAccess),
     ElementaryTypeNameExpression(ElementaryTypeNameExpression),
     TupleExpression(TupleExpression),
+    // This is the same as ElementaryTypeNameExpression
+    // So it will never actually be reached by serde
     NewExpression(NewExpression),
 
     #[serde(rename_all = "camelCase")]
