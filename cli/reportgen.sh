d813a259cb2e8041356ee43db3f6b637a0881e14
TilakMaddy <tilak.madichetti@gmail.com>
2024-01-13 12:30:26 +0530

    add archive to tracking

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 cargo run -- ./tests/contract-playground && cargo run -- ./tests/contract-playground -o report.json && cargo run --bin bot_example -- -p orig_
+
+touch aderyn_pilot/archive.zip
+rm aderyn_pilot/archive.zip
+zip -r9 aderyn_pilot/archive.zip aderyn_pilot/bot_starter_pack -x "aderyn_pilot/bot_starter_pack/target/*" -x "aderyn_pilot/bot_starter_pack/.git/*"
