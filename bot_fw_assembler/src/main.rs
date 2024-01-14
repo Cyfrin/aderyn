90d90d9250221a00f0b69aba6bd48a28f7f2a1c8
TilakMaddy <tilak.madichetti@gmail.com>
2024-01-14 11:37:33 +0530

    supporting dev + prod mode for assembler

--- bot_fw_assembler/src/main.rs
+++ bot_fw_assembler/src/main.rs
@@ -86,71 +86,46 @@ fn main() {
 
             to_insert_content_lines[hook as usize] = &new_aderyn_driver_line;
 
-            std::fs::remove_file(PathBuf::from("bot_starter_pack/Cargo.toml")).unwrap();
+            // Replace with temporary content
+            write_to_cargo_toml(to_insert_content_lines.join("\n"));
 
-            let file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open(PathBuf::from("bot_starter_pack/Cargo.toml"))
-                .unwrap();
-            println!("[*] Temporarily manipulating Cargo.toml in bot_starter_pack");
-
-            let mut bw = BufWriter::new(file);
-
-            write!(bw, "{}", to_insert_content_lines.join("\n")).unwrap();
-            drop(bw);
-
-            println!("[*] Packing bytes");
-            // Remove the existing `archive.zip`
-            let _ = std::process::Command::new("touch")
-                .arg("aderyn_pilot/archive.zip")
-                .stdout(Stdio::inherit()) // This will stream the stdout
-                .stderr(Stdio::inherit())
-                .status();
-
-            let _ = std::process::Command::new("rm")
-                .arg("aderyn_pilot/archive.zip")
-                .stdout(Stdio::inherit()) // This will stream the stdout
-                .stderr(Stdio::inherit())
-                .status();
-
-            // Put the new archive which reflects changes made in `bot_starter_pack`
-            let _output = std::process::Command::new("zip")
-                .args([
-                    "-r9",
-                    "aderyn_pilot/archive.zip",
-                    "bot_starter_pack",
-                    "-x",
-                    "bot_starter_pack/target/*",
-                ])
-                .stdout(Stdio::inherit()) // This will stream the stdout
-                .stderr(Stdio::inherit())
-                .status();
-
-            println!("[*] Created archive.zip in bot_starter_pack");
+            // Make archive from new changes for aderyn_pilot's new command to use
+            pack_bytes_and_create_archive();
 
             // Restore the old content in Cargo.toml
-
-            std::fs::remove_file(PathBuf::from("bot_starter_pack/Cargo.toml")).unwrap();
-
-            let file = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(PathBuf::from("bot_starter_pack/Cargo.toml"))
-                .unwrap();
-
-            let mut bw = BufWriter::new(file);
-
-            write!(bw, "{}", old_content).unwrap();
-            println!("[*] Restoring Cargo.toml in bot_starter_pack");
+            write_to_cargo_toml(old_content);
         }
         BotFrameworkEnvironment::Dev {
-            relative_path_to_aderyn_driver: _,
+            relative_path_to_aderyn_driver,
         } => {
-            // TODO: implement
-            println!("Dev version is not ready yet!");
-            println!("Please run `cargo run --bin bot_fw_assembler -- prod");
+            // Manipulate Cargo.toml
+            let old_content = std::fs::read_to_string("bot_starter_pack/Cargo.toml").unwrap();
+
+            let mut hook: isize = -1;
+            for (idx, line) in old_content.lines().enumerate() {
+                if line.starts_with("aderyn_driver") {
+                    hook = idx as isize;
+                    break;
+                }
+            }
+
+            let mut to_insert_content_lines = old_content.lines().collect::<Vec<_>>();
+
+            let new_aderyn_driver_line = format!(
+                "aderyn_driver = {{ path = \"{}\" }}",
+                relative_path_to_aderyn_driver,
+            );
+
+            to_insert_content_lines[hook as usize] = &new_aderyn_driver_line;
+
+            // Replace with temporary content
+            write_to_cargo_toml(to_insert_content_lines.join("\n"));
+
+            // Make archive from new changes for aderyn_pilot's new command to use
+            pack_bytes_and_create_archive();
+
+            // Restore the old content in Cargo.toml
+            write_to_cargo_toml(old_content);
         }
     }
 }
@@ -163,4 +138,50 @@ fn get_currently_coded_version() -> String {
         }
     }
     panic!("This shouldn't happen");
+}
+
+fn write_to_cargo_toml(content: String) {
+    std::fs::remove_file(PathBuf::from("bot_starter_pack/Cargo.toml")).unwrap();
+
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(PathBuf::from("bot_starter_pack/Cargo.toml"))
+        .unwrap();
+
+    let mut bw = BufWriter::new(file);
+
+    write!(bw, "{}", content).unwrap();
+    println!("[*] Restoring Cargo.toml in bot_starter_pack");
+}
+
+fn pack_bytes_and_create_archive() {
+    println!("[*] Packing bytes");
+    // Remove the existing `archive.zip`
+    let _ = std::process::Command::new("touch")
+        .arg("aderyn_pilot/archive.zip")
+        .stdout(Stdio::inherit()) // This will stream the stdout
+        .stderr(Stdio::inherit())
+        .status();
+
+    let _ = std::process::Command::new("rm")
+        .arg("aderyn_pilot/archive.zip")
+        .stdout(Stdio::inherit()) // This will stream the stdout
+        .stderr(Stdio::inherit())
+        .status();
+
+    // Put the new archive which reflects changes made in `bot_starter_pack`
+    let _output = std::process::Command::new("zip")
+        .args([
+            "-r9",
+            "aderyn_pilot/archive.zip",
+            "bot_starter_pack",
+            "-x",
+            "bot_starter_pack/target/*",
+        ])
+        .stdout(Stdio::inherit()) // This will stream the stdout
+        .stderr(Stdio::inherit())
+        .status();
+
+    println!("[*] Created archive.zip in bot_starter_pack");
 }
