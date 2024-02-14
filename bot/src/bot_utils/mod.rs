721efe7e18b8757665619cad6f4fdde88ca72d8d
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-02-14 21:49:47 +0530

    Tech Debt - Bot code contains old assumptions, now made new (#160)
    Done :)

--- bot/src/bot_utils/mod.rs
+++ bot/src/bot_utils/mod.rs
@@ -8,7 +8,7 @@ pub struct Launchable; // Contract has been given + detectors are attached
 pub struct TestsTarget<Stage = Grounded> {
     stage: std::marker::PhantomData<Stage>,
     pub filepath: String,
-    pub detectors: Vec<Box<dyn IssueDetector>>,
+    pub issue_detectors: Vec<Box<dyn IssueDetector>>,
     pub reusables_detectors: Vec<Box<dyn ReusableDetector>>,
 }
 
@@ -17,19 +17,19 @@ impl TestsTarget<Grounded> {
         Self {
             stage: std::marker::PhantomData::<Grounded>,
             filepath: filepath.to_string(),
-            detectors: vec![],
+            issue_detectors: vec![],
             reusables_detectors: vec![],
         }
     }
 
     pub fn with_issue_detector(self, detector: Box<dyn IssueDetector>) -> TestsTarget<Launchable> {
-        let mut detectors_so_far = self.detectors;
+        let mut detectors_so_far = self.issue_detectors;
         detectors_so_far.push(detector);
 
         TestsTarget::<Launchable> {
             stage: std::marker::PhantomData::<Launchable>,
             filepath: self.filepath,
-            detectors: detectors_so_far,
+            issue_detectors: detectors_so_far,
             reusables_detectors: self.reusables_detectors,
         }
     }
@@ -44,7 +44,7 @@ impl TestsTarget<Grounded> {
         TestsTarget::<Launchable> {
             stage: std::marker::PhantomData::<Launchable>,
             filepath: self.filepath,
-            detectors: self.detectors,
+            issue_detectors: self.issue_detectors,
             reusables_detectors: detectors_so_far,
         }
     }
@@ -52,12 +52,12 @@ impl TestsTarget<Grounded> {
 
 impl TestsTarget<Launchable> {
     pub fn with_issue_detector(self, detector: Box<dyn IssueDetector>) -> TestsTarget<Launchable> {
-        let mut detectors_so_far = self.detectors;
+        let mut detectors_so_far = self.issue_detectors;
         detectors_so_far.push(detector);
         TestsTarget::<Launchable> {
             stage: std::marker::PhantomData::<Launchable>,
             filepath: self.filepath,
-            detectors: detectors_so_far,
+            issue_detectors: detectors_so_far,
             reusables_detectors: self.reusables_detectors,
         }
     }
@@ -71,27 +71,25 @@ impl TestsTarget<Launchable> {
         TestsTarget::<Launchable> {
             stage: std::marker::PhantomData::<Launchable>,
             filepath: self.filepath,
-            detectors: self.detectors,
+            issue_detectors: self.issue_detectors,
             reusables_detectors: detectors_so_far,
         }
     }
 }
 
-// For now, let's assume title() is unique for each detector -
-// TODO later: implement an id method on detectors
-pub type DetectorId = String;
+pub type DetectorName = String;
 pub type ContractJSONFilepath = String;
 pub struct TestsConfig {
-    store: HashMap<DetectorId, Vec<ContractJSONFilepath>>,
+    store: HashMap<DetectorName, Vec<ContractJSONFilepath>>,
 }
 
 impl From<Vec<TestsTarget<Launchable>>> for TestsConfig {
     fn from(tests_targets: Vec<TestsTarget<Launchable>>) -> Self {
-        let mut store: HashMap<DetectorId, Vec<ContractJSONFilepath>> = HashMap::default();
+        let mut store: HashMap<DetectorName, Vec<ContractJSONFilepath>> = HashMap::default();
 
         tests_targets.iter().for_each(|t| {
             let filepath = t.filepath.to_string();
-            t.detectors.iter().for_each(|d| {
+            t.issue_detectors.iter().for_each(|d| {
                 if let Some(ptr) = store.get_mut(&d.name()) {
                     ptr.push(filepath.clone());
                 } else {
@@ -112,7 +110,7 @@ impl From<Vec<TestsTarget<Launchable>>> for TestsConfig {
 }
 
 impl TestsConfig {
-    pub fn get_contracts_for(&self, detector_id: DetectorId) -> Vec<ContractJSONFilepath> {
+    pub fn get_contracts_for(&self, detector_id: DetectorName) -> Vec<ContractJSONFilepath> {
         self.store.get(&detector_id).unwrap_or(&vec![]).clone()
     }
 }
