df3c35c89dd29d8543e1aa71a0c75dafce88a8cc
TilakMaddy <tilak.madichetti@gmail.com>
2024-02-16 10:11:54 +0530

    archive

--- cli/sample_metricsdbgen.sh
+++ cli/sample_metricsdbgen.sh
@@ -1,3 +1,4 @@
+rm "$@"
 cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/mix.json
 cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/neg_high.json
 cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/neg_med.json
