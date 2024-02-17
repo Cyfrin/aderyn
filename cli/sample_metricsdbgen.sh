89a298284b2d5c286f42df33b8c05e4d8279f10c
TilakMaddy <tilak.madichetti@gmail.com>
2024-02-17 11:26:37 +0530

    cleaning root directory by moving judgeops related stuff to judgeops/

--- cli/sample_metricsdbgen.sh
+++ cli/sample_metricsdbgen.sh
@@ -1,5 +1,5 @@
 rm "$@"
-cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/mix.json
-cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/neg_high.json
-cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/neg_med.json
-cargo run --bin judgeops -- -m "$@" apply-judgement tests/sample_feedbacks/pos_med.json
+cargo run --bin judgeops -- -m "$@" apply-judgement judgeops/samples/feedbacks/mix.json
+cargo run --bin judgeops -- -m "$@" apply-judgement judgeops/samples/feedbacks/neg_high.json
+cargo run --bin judgeops -- -m "$@" apply-judgement judgeops/samples/feedbacks/neg_med.json
+cargo run --bin judgeops -- -m "$@" apply-judgement judgeops/samples/feedbacks/pos_med.json
