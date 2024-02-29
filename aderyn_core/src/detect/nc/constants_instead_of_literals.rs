afbd08ed80d786112410844c2f2e3de6855a6e47
Alex Roan <alex.roan@hotmail.com>
2024-02-29 16:42:02 +0000

    Fix Detector: constants instead of literals improvements (#199)

--- aderyn_core/src/detect/nc/constants_instead_of_literals.rs
+++ aderyn_core/src/detect/nc/constants_instead_of_literals.rs
@@ -1,9 +1,15 @@
-use std::{collections::BTreeMap, error::Error};
+use std::{
+    collections::{BTreeMap, HashMap},
+    error::Error,
+};
 
 use crate::{
-    ast::{LiteralKind, NodeID},
+    ast::{Literal, LiteralKind, NodeID},
     capture,
-    context::{browser::ExtractLiterals, workspace_context::WorkspaceContext},
+    context::{
+        browser::{ExtractFunctionDefinitions, ExtractLiterals, ExtractModifierDefinitions},
+        workspace_context::{ASTNode, WorkspaceContext},
+    },
     detect::detector::{IssueDetector, IssueDetectorNamePool, IssueSeverity},
 };
 use eyre::Result;
@@ -16,22 +22,86 @@ pub struct ConstantsInsteadOfLiteralsDetector {
 
 impl IssueDetector for ConstantsInsteadOfLiteralsDetector {
     fn detect(&mut self, context: &WorkspaceContext) -> Result<bool, Box<dyn Error>> {
-        // get all function definitions.
-        // for each function definition, find all Literal types
-        // if the literal type is either a Number, HexString or Address, then add it to the list of found literals
-        for function_definition in context.function_definitions() {
-            ExtractLiterals::from(function_definition)
+        // Get all contracts
+        // For each contract
+        //      Get all function definitions (and to the same for modifiers)
+        //          Get all literals
+        //          For each literal
+        //              if literal.value is not 0 or 1
+        //                  if the literal.value appears more than once, then capture all instances
+
+        for contract in context.contract_definitions() {
+            let mut literal_values_found: HashMap<String, Vec<Literal>> = HashMap::new();
+
+            for function in ExtractFunctionDefinitions::from(contract)
                 .extracted
-                .iter()
-                .for_each(|literal| {
+                .into_iter()
+            {
+                for literal in ExtractLiterals::from(&function).extracted.into_iter() {
                     if (literal.kind == LiteralKind::Number
-                        && literal.value != Some(String::from("0")))
+                        && literal.value != Some(String::from("0"))
+                        && literal.value != Some(String::from("1")))
+                        && literal.value != Some(String::from("2"))
                         || literal.kind == LiteralKind::HexString
                         || literal.kind == LiteralKind::Address
                     {
+                        // If the literal is used as an index access in a variable, don't capture it
+                        if let Some(ASTNode::IndexAccess(_)) = context.get_parent(literal.id) {
+                            continue;
+                        }
+
+                        if let Some(literal_value) = literal.value.as_ref() {
+                            if literal_values_found.contains_key(literal_value) {
+                                literal_values_found
+                                    .get_mut(literal_value)
+                                    .unwrap()
+                                    .push(literal);
+                            } else {
+                                literal_values_found.insert(literal_value.clone(), vec![literal]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for modifier in ExtractModifierDefinitions::from(contract)
+                .extracted
+                .into_iter()
+            {
+                for literal in ExtractLiterals::from(&modifier).extracted.into_iter() {
+                    if (literal.kind == LiteralKind::Number
+                        && literal.value != Some(String::from("0"))
+                        && literal.value != Some(String::from("1")))
+                        && literal.value != Some(String::from("2"))
+                        || literal.kind == LiteralKind::HexString
+                        || literal.kind == LiteralKind::Address
+                    {
+                        // If the literal is used as an index access in a variable, don't capture it
+                        if let Some(ASTNode::IndexAccess(_)) = context.get_parent(literal.id) {
+                            continue;
+                        }
+
+                        if let Some(literal_value) = literal.value.as_ref() {
+                            if literal_values_found.contains_key(literal_value) {
+                                literal_values_found
+                                    .get_mut(literal_value)
+                                    .unwrap()
+                                    .push(literal);
+                            } else {
+                                literal_values_found.insert(literal_value.clone(), vec![literal]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (_, literals) in literal_values_found.iter() {
+                if literals.len() > 1 {
+                    for literal in literals {
                         capture!(self, context, literal);
                     }
-                });
+                }
+            }
         }
 
         Ok(!self.found_instances.is_empty())
@@ -66,15 +136,16 @@ mod constants_instead_of_literals_tests {
 
     #[test]
     fn test_constants_instead_of_literals() {
-        let context =
-            load_contract("../tests/contract-playground/out/Counter.sol/Counter.0.8.21.json");
+        let context = load_contract(
+            "../tests/contract-playground/out/ConstantsLiterals.sol/ConstantsLiterals.json",
+        );
 
         let mut detector = ConstantsInsteadOfLiteralsDetector::default();
         // assert that the detector finds the public function
         let found = detector.detect(&context).unwrap();
         assert!(found);
         // assert that the detector finds the correct number of instances
-        assert_eq!(detector.instances().len(), 1);
+        assert_eq!(detector.instances().len(), 8);
         // assert that the detector returns the correct severity
         assert_eq!(
             detector.severity(),
