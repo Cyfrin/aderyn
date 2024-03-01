da660d2b8febb5888538f28d2b25edaa02937ae6
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-01 07:29:20 +0530

    rebased

--- aderyn_core/src/detect/experimental/silly_capture.rs
+++ aderyn_core/src/detect/experimental/silly_capture.rs
@@ -11,7 +11,7 @@ use eyre::Result;
 #[derive(Default)]
 pub struct SillyCaptureDetector {
     // Keys are source file name and line number
-    found_instances: BTreeMap<(String, usize), NodeID>,
+    found_instances: BTreeMap<(String, usize, String), NodeID>,
 }
 
 impl IssueDetector for SillyCaptureDetector {
@@ -47,7 +47,7 @@ impl IssueDetector for SillyCaptureDetector {
         IssueSeverity::NC
     }
 
-    fn instances(&self) -> BTreeMap<(String, usize), NodeID> {
+    fn instances(&self) -> BTreeMap<(String, usize, String), NodeID> {
         self.found_instances.clone()
     }
 
