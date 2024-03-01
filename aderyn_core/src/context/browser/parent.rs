514f3c9457262f239dc0e099f588df90d92de1f4
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-01 07:09:00 +0530

    cloned -> reference

--- aderyn_core/src/context/browser/parent.rs
+++ aderyn_core/src/context/browser/parent.rs
@@ -5,211 +5,211 @@ use crate::{
 
 pub trait GetImmediateParent {
     /// Get the parent Chain of an ASTNode
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode>;
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode>;
 }
 
 impl GetImmediateParent for Assignment {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for BinaryOperation {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for Block {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for Conditional {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ContractDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ElementaryTypeNameExpression {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for EnumDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for EnumValue {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for EventDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ErrorDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for FunctionCall {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for FunctionCallOptions {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for FunctionDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ForStatement {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for Identifier {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for IdentifierPath {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for IfStatement {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ImportDirective {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for IndexAccess {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for IndexRangeAccess {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for InheritanceSpecifier {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for InlineAssembly {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for Literal {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for MemberAccess {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for NewExpression {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ModifierDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ModifierInvocation {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for OverrideSpecifier {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for ParameterList {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for PragmaDirective {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for Return {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for SourceUnit {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for StructDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for StructuredDocumentation {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for TupleExpression {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for UnaryOperation {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for UserDefinedValueTypeDefinition {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for UsingForDirective {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for VariableDeclaration {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for VariableDeclarationStatement {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
 impl GetImmediateParent for WhileStatement {
-    fn parent(&self, context: &WorkspaceContext) -> Option<ASTNode> {
-        context.get_parent(self.id).cloned()
+    fn parent<'a>(&self, context: &'a WorkspaceContext) -> Option<&'a ASTNode> {
+        context.get_parent(self.id)
     }
 }
