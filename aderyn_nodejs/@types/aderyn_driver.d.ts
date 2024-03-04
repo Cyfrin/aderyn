4100ab17a59f4aae3f0f72b931e836e78fc88aef
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-04 20:56:18 +0530

    declaration ts file

---
+++ aderyn_nodejs/@types/aderyn_driver.d.ts
@@ -0,0 +1,22 @@
+declare module 'aderyn_driver' {
+
+    export function drive(
+        root: string,
+        output: string,
+        no_snippets: boolean,
+        exclude: string[],
+        scope: string[],
+    ): boolean;
+
+    export function drive_with(
+        root: string,
+        output: string,
+        no_snippets: boolean,
+        exclude: string[],
+        scope: string[],
+        js_detector_names: string[]
+    ): boolean;
+
+    export function get_all_issue_detectors_names(): string[]
+
+}
