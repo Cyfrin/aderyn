0b8e5f0613c872016cf4285cfd965e6bb762ce47
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-10 15:23:22 +0530

    detector tested

---
+++ tests/contract-playground/src/HugeConstants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HugeConstants {
+    // Define constant values with huge multiples of 10
+    uint256 constant public HUGE_NUMBER_1 = 1000000000000000000000; // 10^18
+    uint256 constant public HUGE_NUMBER_2 = 10000000000000000000000; // 10^19
+    uint256 constant public HUGE_NUMBER_3 = 100000000000000000000000; // 10^20
+    uint256 constant public HUGE_NUMBER_4 = 1e20;
+    
+}
