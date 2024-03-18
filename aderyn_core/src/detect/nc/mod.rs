7c44e4abd47469538ccc81045ad1621a75073d34
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-03-18 21:46:28 +0530

    New detector: Internal functions called only once can be inlined (#213)
    Co-authored-by: Alex Roan <alex@cyfrin.io>

--- aderyn_core/src/detect/nc/mod.rs
+++ aderyn_core/src/detect/nc/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod constants_instead_of_literals;
 pub(crate) mod non_reentrant_before_others;
 pub(crate) mod require_with_string;
 pub(crate) mod unindexed_events;
+pub(crate) mod useless_internal_function;
 pub(crate) mod useless_public_function;
 pub(crate) mod zero_address_check;
 
@@ -9,5 +10,6 @@ pub use constants_instead_of_literals::ConstantsInsteadOfLiteralsDetector;
 pub use non_reentrant_before_others::NonReentrantBeforeOthersDetector;
 pub use require_with_string::RequireWithStringDetector;
 pub use unindexed_events::UnindexedEventsDetector;
+pub use useless_internal_function::UselessInternalFunctionDetector;
 pub use useless_public_function::UselessPublicFunctionDetector;
 pub use zero_address_check::ZeroAddressCheckDetector;
