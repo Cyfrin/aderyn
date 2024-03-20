03ed66f6a52c85c202c1d18a50d4a2525e75b173
Alex Roan <alex.roan@hotmail.com>
2024-03-20 00:32:10 +0000

    Multiple detector improvements (#244)

--- tests/contract-playground/src/ZeroAddressCheck.sol
+++ tests/contract-playground/src/ZeroAddressCheck.sol
@@ -9,45 +9,45 @@ contract ZeroAddressCheck {
     address public addr;
     IERC20 public token;
 
-    function goodAddress1(address newAddr) public {
+    function goodAddress1(address newAddr) external {
         if (newAddr == address(0)) revert();
         addr = newAddr;
     }
 
-    function goodAddress2(address newAddr) public {
+    function goodAddress2(address newAddr) external {
         require(newAddr != address(0), "Address cannot be zero");
         addr = newAddr;
     }
 
-    function goodToken1(address newAddr) public {
+    function goodToken1(address newAddr) external {
         if (newAddr == address(0)) revert();
         token = IERC20(newAddr);
     }
 
-    function goodToken2(address newAddr) public {
+    function goodToken2(address newAddr) external {
         require(newAddr != address(0), "Address cannot be zero");
         token = IERC20(newAddr);
     }
 
-    function goodToken3(IERC20 newToken) public {
+    function goodToken3(IERC20 newToken) external {
         require(address(newToken) != address(0), "Address cannot be zero");
         token = newToken;
     }
 
-    function goodToken4(IERC20 newToken) public {
+    function goodToken4(IERC20 newToken) external {
         if (address(newToken) == address(0)) revert();
         token = newToken;
     }
 
-    function bad1(address newAddr) public {
+    function bad1(address newAddr) external {
         addr = newAddr;
     }
 
-    function bad2(IERC20 newToken) public {
+    function bad2(IERC20 newToken) external {
         token = newToken;
     }
 
-    function bad3(address newAddr) public {
+    function bad3(address newAddr) external {
         token = IERC20(newAddr);
     }
 }
