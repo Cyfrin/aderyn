829745ea5af042f524fab96f0a279b001cca5a5e
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-03-25 17:22:38 +0530

    Speed up cli/reportgen.sh by 10x (#246)
    Co-authored-by: Alex Roan <alex.roan@hotmail.com>

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+cd tests/contract-playground
+forge build --ast
+cd ../../
+
+export ADERYN_SKIP_BUILD=1
+
 #### MARKDOWN REPORTS ######
 
 # Basic report.md 
-cargo run -- ./tests/contract-playground
+cargo run -- ./tests/contract-playground &
 
 # Create report-config.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json -o report-config.md ./tests/contract-playground/ 
+cargo run -- --config-file ./tests/aderyn.config.json -o report-config.md ./tests/contract-playground/  &
 
 # Create report.judge.md 
-cargo run -- ./tests/contract-playground -o judgeops/current/report.judge.md
+cargo run -- ./tests/contract-playground -o judgeops/current/report.judge.md &
 
 # Create report-config.judge.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json ./tests/contract-playground -o judgeops/current/report-config.judge.md
+cargo run -- --config-file ./tests/aderyn.config.json ./tests/contract-playground -o judgeops/current/report-config.judge.md &
 
 
 ##### JSON REPORTS ########
 
 # Basic report.json
-cargo run -- ./tests/contract-playground -o report.json 
+cargo run -- ./tests/contract-playground -o report.json &
 
 # Create report-config.json based on config file
-cargo run -- --config-file ./tests/aderyn.config.json -o report-config.json ./tests/contract-playground/ 
+cargo run -- --config-file ./tests/aderyn.config.json -o report-config.json ./tests/contract-playground/  &
 
+wait
+
+unset ADERYN_SKIP_BUILD=1
 
 #### BOT ###################
 
@@ -33,3 +42,4 @@ cargo run --bin bot_example -- -p orig_
 
 cli/bot_archivegen.sh
 cli/sample_metricsdbgen.sh judgeops/samples/sample_db.json
+
