a121abb80d3d13ed1c688dde4547d7a04253a882
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-27 11:28:04 +0530

    done for the rest

--- aderyn_core/src/visitor/ast_visitor.rs
+++ aderyn_core/src/visitor/ast_visitor.rs
@@ -504,15 +504,17 @@ pub trait ASTConstVisitor {
 }
 
 pub trait Node {
+    /// [`Node::accept`] is designed to propagate
     fn accept(&self, _visitor: &mut impl ASTConstVisitor) -> Result<()> {
         Ok(())
     }
+    /// [`Node::accept_metadata`] is designed to propagate into the AST subtree
+    /// although it doesn't happen by itself. [`Node::accept`] triggers the propagation
     fn accept_metadata(&self, _visitor: &mut impl ASTConstVisitor) -> Result<()> {
         Ok(())
     }
     /// [`Node::accept_id`] is not designed to propagate into the AST subtree
-    fn accept_id(&self, visitor: &mut impl ASTConstVisitor) -> Result<()> {
-        visitor.visit_node_id(None)?;
+    fn accept_id(&self, _visitor: &mut impl ASTConstVisitor) -> Result<()> {
         Ok(())
     }
 }
