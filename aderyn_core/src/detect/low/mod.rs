3eee90127bd0eaf1b62244505b91fdd6318eb088
TilakMaddy <tilak.madichetti@gmail.com>
2024-03-28 16:04:16 +0530

    unprotected detector added

--- aderyn_core/src/detect/low/mod.rs
+++ aderyn_core/src/detect/low/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod avoid_abi_encode_packed;
 pub(crate) mod deprecated_oz_functions;
 pub(crate) mod ecrecover;
 pub(crate) mod push_0_opcode;
+pub(crate) mod unprotected_init_function;
 pub(crate) mod unsafe_erc20_functions;
 pub(crate) mod unspecific_solidity_pragma;
 
@@ -9,5 +10,6 @@ pub use avoid_abi_encode_packed::AvoidAbiEncodePackedDetector;
 pub use deprecated_oz_functions::DeprecatedOZFunctionsDetector;
 pub use ecrecover::EcrecoverDetector;
 pub use push_0_opcode::PushZeroOpcodeDetector;
+pub use unprotected_init_function::UnprotectedInitializerDetector;
 pub use unsafe_erc20_functions::UnsafeERC20FunctionsDetector;
 pub use unspecific_solidity_pragma::UnspecificSolidityPragmaDetector;
