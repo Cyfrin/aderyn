91f84c5f63cae729def8753ace39285b4b51640a
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-04 10:29:47 +0530

    generalized peek() impl for all node

--- aderyn_core/src/context/browser/peek.rs
+++ aderyn_core/src/context/browser/peek.rs
@@ -1,12 +1,35 @@
-use crate::context::workspace_context::{ASTNode, WorkspaceContext};
+use crate::{
+    ast::NodeID,
+    context::workspace_context::WorkspaceContext,
+    visitor::ast_visitor::{ASTConstVisitor, Node},
+};
 
 pub trait Peek {
     /// Peek into the source code of a node
     fn peek(&self, context: &WorkspaceContext) -> Option<String>;
 }
 
-impl Peek for ASTNode {
-    fn peek(&self, context: &WorkspaceContext) -> Option<String> {
-        context.get_source_code_of_node(self.id()?)
+#[derive(Default)]
+struct NodeIDReceiver {
+    id: Option<NodeID>,
+}
+
+impl ASTConstVisitor for NodeIDReceiver {
+    fn visit_node_id(&mut self, node_id: Option<NodeID>) -> eyre::Result<()> {
+        self.id = node_id;
+        Ok(())
+    }
+}
+
+impl<T: Node + ?Sized> Peek for T {
+    fn peek<'a>(&self, context: &'a WorkspaceContext) -> Option<String> {
+        // Setup a Node ID receiver
+        let mut node_id_receiver = NodeIDReceiver::default();
+
+        // Find the ID of the node this method is called upon
+        self.accept_id(&mut node_id_receiver).ok()?;
+        let current_node_id = node_id_receiver.id?;
+
+        context.get_source_code_of_node(current_node_id)
     }
 }
