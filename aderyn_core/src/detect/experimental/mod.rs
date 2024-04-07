753cbeffd8ecc6a9489ea2599df322130bce51cb
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-07 19:46:55 +0530

    demo example added

--- aderyn_core/src/detect/experimental/mod.rs
+++ aderyn_core/src/detect/experimental/mod.rs
@@ -2,3 +2,4 @@ pub(crate) mod closest_parent;
 pub(crate) mod immediate_children;
 pub(crate) mod immediate_parent;
 pub(crate) mod parent_chain;
+pub(crate) mod sibling;
---
+++ aderyn_core/src/detect/experimental/sibling.rs
@@ -0,0 +1,73 @@
+use std::{collections::BTreeMap, error::Error};
+
+use crate::{
+    ast::NodeID,
+    context::{
+        browser::{
+            GetImmediateChildren, GetNextSibling, GetPreviousSibling, SortNodeReferencesToSequence,
+        },
+        workspace_context::WorkspaceContext,
+    },
+    detect::detector::{IssueDetector, IssueDetectorNamePool, IssueSeverity},
+};
+use eyre::Result;
+
+#[derive(Default)]
+pub struct SiblingDemonstrator {
+    // Keys are source file name and line number
+    found_instances: BTreeMap<(String, usize, String), NodeID>,
+}
+
+impl IssueDetector for SiblingDemonstrator {
+    fn detect(&mut self, context: &WorkspaceContext) -> Result<bool, Box<dyn Error>> {
+        for contract in context.contract_definitions() {
+            if let Some(children) = contract.children(context) {
+                if let Some(sorted) = children.sort_by_line_nos(context) {
+                    assert!(sorted.len() >= 2);
+                    assert!(sorted[1].previous_sibling(context).unwrap() == sorted[0]);
+                    assert!(sorted[0].next_sibling(context).unwrap() == sorted[1]);
+                }
+            }
+        }
+
+        Ok(!self.found_instances.is_empty())
+    }
+
+    fn severity(&self) -> IssueSeverity {
+        IssueSeverity::Medium
+    }
+
+    fn title(&self) -> String {
+        String::from("Sibling Demonstration")
+    }
+
+    fn description(&self) -> String {
+        String::from("Sibling Demonstration")
+    }
+
+    fn instances(&self) -> BTreeMap<(String, usize, String), NodeID> {
+        self.found_instances.clone()
+    }
+
+    fn name(&self) -> String {
+        format!("{}", IssueDetectorNamePool::CentralizationRisk)
+    }
+}
+
+#[cfg(test)]
+mod sibling_demo_tests {
+    use crate::detect::{
+        detector::{detector_test_helpers::load_contract, IssueDetector},
+        experimental::sibling::SiblingDemonstrator,
+    };
+
+    #[test]
+    fn test_siblings() {
+        let context = load_contract(
+            "../tests/contract-playground/out/StorageConditionals.sol/StorageConditionals.json",
+        );
+
+        let mut detector = SiblingDemonstrator::default();
+        let _ = detector.detect(&context).unwrap();
+    }
+}
