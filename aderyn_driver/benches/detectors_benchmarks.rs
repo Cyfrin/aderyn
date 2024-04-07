56d3b34901602bb91bd358ce4d1c58ece533d27b
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-07 18:23:28 +0530

    detectors benchmarks

--- aderyn_driver/benches/detectors_benchmarks.rs
+++ aderyn_driver/benches/detectors_benchmarks.rs
@@ -1,35 +1,21 @@
 use std::path::PathBuf;
 
-use aderyn_driver::{
-    detection_modules::nc::UselessPublicFunctionDetector, detector::IssueDetector,
-};
+use aderyn_driver::detector::get_all_issue_detectors;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-/*  Simple TEMPLATE to do benchmark */
-
-// fn hello_world_bench(c: &mut Criterion) {
-//     c.bench_function("hello_world", |b| {
-//         b.iter(|| {
-//             println!("Hello World!");
-//         })
-//     });
-// }
-
-// criterion_group!(hello_world, hello_world_bench);
-// criterion_main!(hello_world);
-
-fn bench_useless_public_function_detector(c: &mut Criterion) {
+fn bench_on_contract_playground(c: &mut Criterion) {
     let root_path = PathBuf::from("../tests/contract-playground");
     let (_, context) = aderyn_driver::with_project_root_at(&root_path, &None, &None);
-    let mut detector = UselessPublicFunctionDetector::default();
 
-    c.bench_function(detector.name().as_str(), |b| {
-        b.iter(|| {
-            let _ = detector.detect(&context).unwrap();
-        })
-    });
+    for mut detector in get_all_issue_detectors() {
+        c.bench_function(detector.name().as_str(), |b| {
+            b.iter(|| {
+                let _ = detector.detect(&context).unwrap();
+            })
+        });
+    }
 }
 
-criterion_group!(nc, bench_useless_public_function_detector);
-criterion_main!(nc);
+criterion_group!(detectors, bench_on_contract_playground);
+criterion_main!(detectors);
