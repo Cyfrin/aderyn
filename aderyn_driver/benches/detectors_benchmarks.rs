586010b6561f6c03a2af73f99aee28233dbea17c
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-13 09:18:59 +0530

    benchmarking script

--- aderyn_driver/benches/detectors_benchmarks.rs
+++ aderyn_driver/benches/detectors_benchmarks.rs
@@ -1,10 +1,13 @@
 use std::path::PathBuf;
 
-use aderyn_driver::detector::get_all_issue_detectors;
+use aderyn_driver::{
+    detector::get_all_issue_detectors,
+    driver::{drive, Args},
+};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn bench_on_contract_playground(c: &mut Criterion) {
+fn bench_individual_detectors_on_contract_playground(c: &mut Criterion) {
     let root_path = PathBuf::from("../tests/contract-playground");
     let (_, context) = aderyn_driver::with_project_root_at(&root_path, &None, &None);
 
@@ -17,5 +20,26 @@ fn bench_on_contract_playground(c: &mut Criterion) {
     }
 }
 
-criterion_group!(detectors, bench_on_contract_playground);
+fn bench_aderyn_on_contract_playground(c: &mut Criterion) {
+    let root_path = PathBuf::from("../tests/contract-playground");
+
+    c.bench_function("aderyn", |b| {
+        b.iter(|| {
+            drive(Args {
+                root: root_path.to_string_lossy().to_string(),
+                output: String::from("aderyn-report-for-bench.md"),
+                no_snippets: false,
+                exclude: None,
+                scope: None,
+                stdout: false,
+            });
+        });
+    });
+}
+
+criterion_group!(
+    detectors,                                         // Group name is the first argument
+    bench_aderyn_on_contract_playground,               // Group 1
+    bench_individual_detectors_on_contract_playground, // Group 2
+);
 criterion_main!(detectors);
