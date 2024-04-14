41e6de1d26d2c8de49bb80999f8e97f7791e46e2
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-14 18:18:00 +0530

    reportgen

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-cd tests/contract-playground
-forge build --ast
-FOUNDRY_PROFILE=sample forge build --ast
-cd ../../
-
-export ADERYN_SKIP_BUILD=1
-
 #### MARKDOWN REPORTS ######
 
 # Basic report.md 
 cargo run --  --scope src/ --exclude lib/ ./tests/contract-playground &
 
 # Create report-config.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json --scope src/ --exclude lib/ -o report-config.md ./tests/contract-playground/  &
+cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o report-config.md ./tests/contract-playground/  &
 
 # Create report.judge.md 
 cargo run --  --scope src/ --exclude lib/ -o judgeops/current/report.judge.md  ./tests/contract-playground &
 
 # Create report-config.judge.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json --scope src/ --exclude lib/ -o judgeops/current/report-config.judge.md ./tests/contract-playground &
+cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o judgeops/current/report-config.judge.md ./tests/contract-playground &
 
 
 ##### JSON REPORTS ########
@@ -28,11 +21,9 @@ cargo run -- --config-file ./tests/aderyn.config.json --scope src/ --exclude lib
 cargo run -- --scope src/ --exclude lib/ -o report.json ./tests/contract-playground  &
 
 # Create report-config.json based on config file
-cargo run -- --config-file ./tests/aderyn.config.json --scope src/ --exclude lib/ -o report-config.json ./tests/contract-playground/  &
+cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o report-config.json ./tests/contract-playground/  &
 
 wait
-
-unset ADERYN_SKIP_BUILD
 
 #### Other scripts #############
 
