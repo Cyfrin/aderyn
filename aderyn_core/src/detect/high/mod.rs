04ccf66480e2b80d31f0f1b01ed1b81afe6bc02c
Alex Roan <alex@cyfrin.io>
2024-04-15 16:59:49 +0100

    Reclassify detectors (#331)

--- aderyn_core/src/detect/high/mod.rs
+++ aderyn_core/src/detect/high/mod.rs
@@ -1,7 +1,11 @@
 pub(crate) mod arbitrary_transfer_from;
+pub(crate) mod avoid_abi_encode_packed;
 pub(crate) mod block_timestamp_deadline;
 pub(crate) mod delegate_call_in_loop;
+pub(crate) mod unprotected_init_function;
 
 pub use arbitrary_transfer_from::ArbitraryTransferFromDetector;
+pub use avoid_abi_encode_packed::AvoidAbiEncodePackedDetector;
 pub use block_timestamp_deadline::BlockTimestampDeadlineDetector;
 pub use delegate_call_in_loop::DelegateCallInLoopDetector;
+pub use unprotected_init_function::UnprotectedInitializerDetector;
