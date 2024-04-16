165ff0fc57e94acfb8cea4d66089411704d6d478
TilakMaddy <tilak.madichetti@gmail.com>
2024-04-16 19:02:09 +0530

    peek abstractions written

--- aderyn_core/src/context/workspace_context.rs
+++ aderyn_core/src/context/workspace_context.rs
@@ -1258,6 +1258,24 @@ impl WorkspaceContext {
         None
     }
 
+    pub fn get_offset_and_length_of_node(&self, node_id: NodeID) -> Option<(usize, usize)> {
+        let node = self.nodes.get(&node_id)?;
+        let src_location = node.src().unwrap_or("");
+
+        let chopped_location = match src_location.rfind(':') {
+            Some(index) => &src_location[..index],
+            None => src_location, // No colon found, return the original string
+        }
+        .to_string();
+
+        if let Some((offset, len)) = chopped_location.split_once(':') {
+            let offset: usize = offset.parse().ok()?;
+            let len: usize = len.parse().ok()?;
+            return Some((offset, len));
+        }
+        None
+    }
+
     pub fn get_source_unit_from_child_node(&self, node: &ASTNode) -> Option<&SourceUnit> {
         let source_unit_id = match node {
             ASTNode::ArrayTypeName(node) => self
