9d777cb9bbe7523ef645639c16b396a0e839c4e2
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2024-04-30 03:29:32 +0100

    feat: add contract

---
+++ tests/contract-playground/src/ArithmeticUnderflowOverflow.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.8.0;
+ 
+contract ArithmeticUnderflowOverflow {
+    uint256 public counter;
+
+    function add(uint256 num1, uint256 num2) public pure returns (uint256 result) {
+      result = num1 + num2;
+    }
+
+    function sub(uint256 num1, uint256 num2) public pure returns (uint256 result) {
+      result = num1 - num2;
+    }
+
+    function incrementCounter() public {
+      counter += 1;
+    }
+
+    function decrementCounter() public {
+      counter += 1;
+    }
+}
