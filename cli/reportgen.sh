e6cb8ec2d6443e783ac5f825fa7fba5e10168f4d
kevincharm <kevin@kevincharm.com>
2024-05-06 21:09:32 +0800

    Fix LargeLiteralValueDetector: handle hex literals & underscore separators (#396)
    Co-authored-by: Alex Roan <alex@cyfrin.io>

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -10,28 +10,28 @@ cd ../../
 #### MARKDOWN REPORTS ######
 
 # Basic report.md 
-cargo run -- ./tests/contract-playground --skip-build --skip-update-check &
+cargo run -- ./tests/contract-playground --skip-update-check &
 
 # Basic report.profile.md 
-FOUNDRY_PROFILE=sample cargo run -- -o report.sample_profile.md ./tests/contract-playground --skip-build --skip-update-check &
+FOUNDRY_PROFILE=sample cargo run -- -o report.sample_profile.md ./tests/contract-playground --skip-update-check &
 
 # Create report-config.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json -o report-config.md ./tests/contract-playground/ --skip-build --skip-update-check  &
+cargo run -- --config-file ./tests/aderyn.config.json -o report-config.md ./tests/contract-playground/ --skip-update-check  &
 
 # Create report.judge.md 
-cargo run -- ./tests/contract-playground -o judgeops/current/report.judge.md --skip-build --skip-update-check &
+cargo run -- ./tests/contract-playground -o judgeops/current/report.judge.md --skip-update-check &
 
 # Create report-config.judge.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json ./tests/contract-playground -o judgeops/current/report-config.judge.md --skip-build --skip-update-check &
+cargo run -- --config-file ./tests/aderyn.config.json ./tests/contract-playground -o judgeops/current/report-config.judge.md --skip-update-check &
 
 
 ##### JSON REPORTS ########
 
 # Basic report.json
-cargo run -- ./tests/contract-playground -o report.json --skip-build --skip-update-check &
+cargo run -- ./tests/contract-playground -o report.json --skip-update-check &
 
 # Create report-config.json based on config file
-cargo run -- --config-file ./tests/aderyn.config.json -o report-config.json ./tests/contract-playground/ --skip-build --skip-update-check &
+cargo run -- --config-file ./tests/aderyn.config.json -o report-config.json ./tests/contract-playground/ --skip-update-check &
 
 wait
 
