60da0d4601f128acd683abd175172d114e11dc84
Alex Roan <alex@cyfrin.io>
2024-05-09 01:14:44 +0100

    remove debt from previous merge (#411)

--- aderyn_core/src/detect/experimental/mod.rs
+++ aderyn_core/src/detect/experimental/mod.rs
@@ -1,6 +1,5 @@
 pub(crate) mod ancestral_line;
 pub(crate) mod closest_ancestor;
-pub(crate) mod get_external_calls;
 pub(crate) mod immediate_children;
 pub(crate) mod immediate_parent;
 pub(crate) mod peek_over;
