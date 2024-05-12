080445bcada40fc356b00c4de93de3076f314140
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-12 13:41:40 +0530

    read config from foundry

--- aderyn_driver/src/lib.rs
+++ aderyn_driver/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod driver;
 pub(crate) mod foundry_compiler_helpers;
+pub(crate) mod foundry_config_helpers;
 pub(crate) mod process_auto;
 pub(crate) mod project_compiler_tests;
 use std::path::Path;
