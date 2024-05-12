b3c249c4c68bf89aae3730de9f82cb2022f57864
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-12 13:41:47 +0530

    read config from foundry

---
+++ aderyn_driver/src/foundry_config_helpers.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use foundry_config::Config;
+
+pub fn derive_from_foundry_toml(
+    root: &Path,
+    scope: &Option<Vec<String>>,
+    exclude: &Option<Vec<String>>,
+    src: &Option<Vec<String>>,
+) -> (
+    Option<Vec<String>>, // Scope
+    Option<Vec<String>>, // Exclude
+    Option<Vec<String>>, // Src
+) {
+    let config = Config::load_with_root(root);
+
+    let libs = config.libraries.clone();
+    let test = config.test.to_string_lossy().to_string();
+
+    let src = match src {
+        Some(src) => src.clone(),
+        None => {
+            vec![config.src.to_string_lossy().to_string()]
+        }
+    };
+
+    let exclude = match exclude {
+        Some(exclude) => exclude.clone(),
+        None => {
+            let mut exclude = vec![];
+            exclude.push(test);
+            exclude.extend(libs);
+            exclude
+        }
+    };
+
+    (scope.clone(), Some(exclude), Some(src))
+}
