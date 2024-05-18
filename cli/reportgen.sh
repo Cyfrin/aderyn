0021501ebf0fc2e4f4f81e8b1c45bef6735e057a
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-18 19:49:51 +0530

    No skip-build option for icf (doesn't make sense)

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -3,13 +3,13 @@
 #### MARKDOWN REPORTS ######
 
 # Basic report.md 
-cargo run --  --scope src/ --exclude lib/ ./tests/contract-playground -o ./reports/report.md --skip-build --skip-update-check &
+cargo run --  --scope src/ --exclude lib/ ./tests/contract-playground -o ./reports/report.md --skip-update-check &
 
 # Create report-config.md based on config file
-cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o ./reports/report-config.md ./tests/contract-playground/ --skip-build --skip-update-check  &
+cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o ./reports/report-config.md ./tests/contract-playground/ --skip-update-check  &
 
 # Adhoc sol files report.md 
-cargo run --  ./tests/adhoc-sol-files -o ./reports/adhoc-sol-files-report.md --skip-build --skip-update-check &
+cargo run --  ./tests/adhoc-sol-files -o ./reports/adhoc-sol-files-report.md --skip-update-check &
 
 # nft-report.md (Handle remappings)
 cd tests/foundry-nft-f23 && forge install && cd ../.. &&
@@ -25,10 +25,10 @@ FOUNDRY_PROFILE=uniswap cargo run tests/contract-playground/ -o ./reports/uniswa
 ##### JSON REPORTS ########
 
 # Basic report.json
-cargo run -- --scope src/ --exclude lib/ -o ./reports/report.json ./tests/contract-playground --skip-build --skip-update-check &
+cargo run -- --scope src/ --exclude lib/ -o ./reports/report.json ./tests/contract-playground --skip-update-check &
 
 # Create report-config.json based on config file
-cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o ./reports/report-config.json ./tests/contract-playground/ --skip-build --skip-update-check &
+cargo run -- --config-file ./tests/aderyn.config.json --exclude lib/ -o ./reports/report-config.json ./tests/contract-playground/ --skip-update-check &
 
 ##### SARIF REPORTS ########
 
