89c811d319a479342b57e87b9f8b3c387b64c94f
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-19 09:14:12 +0530

    imported functions

--- aderyn_core/src/audit/auditor.rs
+++ aderyn_core/src/audit/auditor.rs
@@ -3,11 +3,18 @@ use std::error::Error;
 use prettytable::{format, Row, Table};
 
 use crate::{
-    audit::attack_surface::AttackSurfaceDetector, context::workspace_context::WorkspaceContext,
+    audit::{
+        attack_surface::AttackSurfaceDetector,
+        public_functions_no_sender::PublicFunctionsNoSenderChecksDetector,
+    },
+    context::workspace_context::WorkspaceContext,
 };
 
 pub fn get_auditor_detectors() -> Vec<Box<dyn AuditorDetector>> {
-    vec![Box::<AttackSurfaceDetector>::default()]
+    vec![
+        Box::<AttackSurfaceDetector>::default(),
+        Box::<PublicFunctionsNoSenderChecksDetector>::default(),
+    ]
 }
 
 pub trait AuditorDetector: Send + Sync + 'static {
--- aderyn_core/src/audit/mod.rs
+++ aderyn_core/src/audit/mod.rs
@@ -1,2 +1,3 @@
 pub mod attack_surface;
 pub mod auditor;
+pub mod public_functions_no_sender;
