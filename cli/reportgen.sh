473efffb0cae4d66df6cce6b13bd0fa54eead58a
Alex Roan <alex@cyfrin.io>
2024-05-29 15:41:16 +0100

    Remove Legacy tests

--- cli/reportgen.sh
+++ cli/reportgen.sh
@@ -1,2 +1,35 @@
-cli/icf_reportgen.sh
-cli/legacy_reportgen.sh
+#!/bin/bash
+
+#### MARKDOWN REPORTS ######
+
+# Basic report.md 
+cargo run --  -i src/ -x lib/ ./tests/contract-playground -o ./reports/report.md --skip-update-check &
+
+# Adhoc sol files report.md 
+cargo run --  ./tests/adhoc-sol-files -o ./reports/adhoc-sol-files-report.md --skip-update-check &
+
+# Aderyn.toml with nested root
+cargo run -- ./tests/2024-05-Sablier -o ./reports/sablier-aderyn-toml-nested-root.md --skip-update-check &
+
+# nft-report.md (Handle remappings)
+cd tests/foundry-nft-f23 && forge install && cd ../.. &&
+cargo run --  ./tests/foundry-nft-f23 -i src/ -x lib/ -o ./reports/nft-report.md --skip-update-check &
+
+# ccip-functions-report.md (Handle remappings)
+cargo run -- tests/ccip-contracts/contracts --src src/v0.8/functions/ -x "tests/,test/,mocks/" -o ./reports/ccip-functions-report.md &
+
+# Extract src, scope and exclude from foundry profile in case of foundry project
+FOUNDRY_PROFILE=uniswap cargo run tests/contract-playground/ -o ./reports/uniswap_profile.md &
+
+
+##### JSON REPORTS ########
+
+# Basic report.json
+cargo run -- -i src/ -x lib/ -o ./reports/report.json ./tests/contract-playground --skip-update-check &
+
+##### SARIF REPORTS ########
+
+# Basic report.sarif
+cargo run -- ./tests/contract-playground -o ./reports/report.sarif --skip-update-check &
+
+wait
