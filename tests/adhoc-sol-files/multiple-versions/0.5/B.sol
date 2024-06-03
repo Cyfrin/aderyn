2e725eea0fdf34e2c31f3389215913d62217dbba
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-06-03 16:06:04 +0530

    Important Fix/support ast parsing from multiple lines  (#496)

---
+++ tests/adhoc-sol-files/multiple-versions/0.5/B.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Vars {
+    address public MY_ADDRESS = address(0);
+    uint256 public MY_UINT = 134131;
+
+    function emptyBlockCatchMe() external {
+        
+    }
+}
