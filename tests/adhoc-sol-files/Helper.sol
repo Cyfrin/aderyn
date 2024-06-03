0470eaacec62b591be4cb719b244ab9aae1f8754
TilakMaddy <tilak.madichetti@gmail.com>
2024-06-03 20:56:09 +0530

    fix continued

--- tests/adhoc-sol-files/Helper.sol
+++ tests/adhoc-sol-files/Helper.sol
@@ -4,4 +4,12 @@ pragma solidity >=0.8.0;
 function make() pure {
 }
 
+// Not used  (should be captured)
 error NotNice();
+
+// Used in below function (should not be captured)
+error OutsideError();
+
+function iLiveOutsideContracts() {
+    revert OutsideError();
+}
