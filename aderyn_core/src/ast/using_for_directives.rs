f9b6f118ee1b3527391582feeb0f2f1161c9e2ef
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-06-13 14:43:42 +0530

    AST Improvements + Adding prb math to CI  (#513)
    Co-authored-by: Alex Roan <alex@cyfrin.io>

--- aderyn_core/src/ast/using_for_directives.rs
+++ aderyn_core/src/ast/using_for_directives.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::visitor::ast_visitor::*;
+// use cyfrin_foundry_compilers::artifacts::serde_helpers;
 use eyre::Result;
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
@@ -7,17 +8,53 @@ use std::fmt::Display;
 #[derive(Clone, Debug, Deserialize, Eq, Serialize, PartialEq, Hash)]
 #[serde(rename_all = "camelCase")]
 pub struct UsingForDirective {
-    pub library_name: IdentifierPath,
+    pub function_list: Option<Vec<UsingForFunctionItem>>,
+    #[serde(default)]
+    pub global: bool,
+    pub library_name: Option<UserDefinedTypeNameOrIdentifierPath>,
     pub type_name: Option<TypeName>,
     pub src: String,
     pub id: NodeID,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+#[serde(untagged)]
+pub enum UserDefinedTypeNameOrIdentifierPath {
+    UserDefinedTypeName(UserDefinedTypeName),
+    IdentifierPath(IdentifierPath),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+#[serde(untagged)]
+pub enum UsingForFunctionItem {
+    Function(FunctionIdentifierPath),
+    OverloadedOperator(OverloadedOperator),
+}
+
+/// A wrapper around [IdentifierPath] for the [UsingForDirective].
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+pub struct FunctionIdentifierPath {
+    pub function: IdentifierPath,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Hash)]
+pub struct OverloadedOperator {
+    pub definition: IdentifierPath,
+    pub operator: String,
 }
 
 impl Node for UsingForDirective {
     fn accept(&self, visitor: &mut impl ASTConstVisitor) -> Result<()> {
         if visitor.visit_using_for_directive(self)? {
             // TODO there is a deviation. Missing FuntionsOrLibrary
-            self.library_name.accept(visitor)?;
+            if self.library_name.is_some() {
+                match self.library_name.as_ref().unwrap() {
+                    UserDefinedTypeNameOrIdentifierPath::UserDefinedTypeName(_) => {}
+                    UserDefinedTypeNameOrIdentifierPath::IdentifierPath(identifier_path) => {
+                        identifier_path.accept(visitor)?;
+                    }
+                };
+            }
             if self.type_name.is_some() {
                 self.type_name.as_ref().unwrap().accept(visitor)?;
             }
@@ -26,7 +63,14 @@ impl Node for UsingForDirective {
         visitor.end_visit_using_for_directive(self)
     }
     fn accept_metadata(&self, visitor: &mut impl ASTConstVisitor) -> Result<()> {
-        visitor.visit_immediate_children(self.id, vec![self.library_name.id])?;
+        if self.library_name.is_some() {
+            match self.library_name.as_ref().unwrap() {
+                UserDefinedTypeNameOrIdentifierPath::UserDefinedTypeName(_) => {}
+                UserDefinedTypeNameOrIdentifierPath::IdentifierPath(identifier_path) => {
+                    visitor.visit_immediate_children(self.id, vec![identifier_path.id])?;
+                }
+            };
+        }
         Ok(())
     }
     fn accept_id(&self, visitor: &mut impl ASTConstVisitor) -> Result<()> {
@@ -38,7 +82,7 @@ impl Node for UsingForDirective {
 impl Display for UsingForDirective {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!(
-            "using {} for {}",
+            "using {:?} for {}",
             self.library_name,
             match self.type_name.as_ref() {
                 Some(type_name) => format!("{type_name}"),
