da4e0e0ea9f85b56fe4bc82be0e63a86dd0bcc3c
Alex Roan <alex@cyfrin.io>
2024-06-28 11:35:02 +0100

    clippy

--- aderyn_core/src/detect/high/enumerable_loop_removal.rs
+++ aderyn_core/src/detect/high/enumerable_loop_removal.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 use std::error::Error;
 
-use crate::ast::{NodeID, NodeType, TypeName};
+use crate::ast::{NodeID, NodeType};
 
 use crate::capture;
 use crate::context::browser::ExtractMemberAccesses;
