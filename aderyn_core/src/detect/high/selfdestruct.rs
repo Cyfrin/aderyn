5b06d92ec019346e7e0d794d1232830b2fa31757
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-07-16 18:28:04 +0530

    New Detector: `selfdestruct`  (#589)
    Co-authored-by: Alex Roan <alex@cyfrin.io>

---
+++ aderyn_core/src/detect/high/selfdestruct.rs
@@ -0,0 +1,81 @@
+use std::collections::BTreeMap;
+use std::error::Error;
+
+use crate::ast::NodeID;
+
+use crate::capture;
+use crate::detect::detector::IssueDetectorNamePool;
+use crate::{
+    context::workspace_context::WorkspaceContext,
+    detect::detector::{IssueDetector, IssueSeverity},
+};
+use eyre::Result;
+
+#[derive(Default)]
+pub struct SelfdestructIdentifierDetector {
+    // Keys are: [0] source file name, [1] line number, [2] character location of node.
+    // Do not add items manually, use `capture!` to add nodes to this BTreeMap.
+    found_instances: BTreeMap<(String, usize, String), NodeID>,
+}
+
+impl IssueDetector for SelfdestructIdentifierDetector {
+    fn detect(&mut self, context: &WorkspaceContext) -> Result<bool, Box<dyn Error>> {
+        for identifier in context.identifiers() {
+            if identifier.name == "selfdestruct" {
+                capture!(self, context, identifier);
+            }
+        }
+        Ok(!self.found_instances.is_empty())
+    }
+
+    fn severity(&self) -> IssueSeverity {
+        IssueSeverity::High
+    }
+
+    fn title(&self) -> String {
+        String::from("Depracated EVM Instruction for `selfdestruct` should not be used.")
+    }
+
+    fn description(&self) -> String {
+        String::from("")
+    }
+
+    fn instances(&self) -> BTreeMap<(String, usize, String), NodeID> {
+        self.found_instances.clone()
+    }
+
+    fn name(&self) -> String {
+        IssueDetectorNamePool::SelfdestructIdentifier.to_string()
+    }
+}
+
+#[cfg(test)]
+mod selfdestruct_identifier_tests {
+    use crate::detect::{detector::IssueDetector, high::SelfdestructIdentifierDetector};
+
+    #[test]
+    fn test_selfdestruct_identifier_tests() {
+        let context = crate::detect::test_utils::load_solidity_source_unit(
+            "../tests/contract-playground/src/UsingSelfdestruct.sol",
+        );
+
+        let mut detector = SelfdestructIdentifierDetector::default();
+        let found = detector.detect(&context).unwrap();
+        // assert that the detector found an issue
+        assert!(found);
+        // assert that the detector found the correct number of instances
+        assert_eq!(detector.instances().len(), 1);
+        // assert the severity is high
+        assert_eq!(
+            detector.severity(),
+            crate::detect::detector::IssueSeverity::High
+        );
+        // assert the title is correct
+        assert_eq!(
+            detector.title(),
+            String::from("Depracated EVM Instruction for `selfdestruct` should not be used.")
+        );
+        // assert the description is correct
+        assert_eq!(detector.description(), String::from(""));
+    }
+}
