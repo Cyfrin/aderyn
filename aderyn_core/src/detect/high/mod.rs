10e77e8404ab76e6f199f87baf1c9d1495365ee9
TilakMaddy <tilak.madichetti@gmail.com>
2024-07-16 16:37:19 +0530

    uninit state vars detector

--- aderyn_core/src/detect/high/mod.rs
+++ aderyn_core/src/detect/high/mod.rs
@@ -9,6 +9,7 @@ pub(crate) mod multiple_constructors;
 pub(crate) mod nested_struct_in_mapping;
 pub(crate) mod reused_contract_name;
 pub(crate) mod storage_array_edit_with_memory;
+pub(crate) mod uninitialized_state_variable;
 pub(crate) mod unprotected_init_function;
 pub(crate) mod unsafe_casting;
 
@@ -23,5 +24,6 @@ pub use multiple_constructors::MultipleConstructorsDetector;
 pub use nested_struct_in_mapping::NestedStructInMappingDetector;
 pub use reused_contract_name::ReusedContractNameDetector;
 pub use storage_array_edit_with_memory::StorageArrayEditWithMemoryDetector;
+pub use uninitialized_state_variable::UninitializedStateVariable;
 pub use unprotected_init_function::UnprotectedInitializerDetector;
 pub use unsafe_casting::UnsafeCastingDetector;
