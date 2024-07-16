61493eb5ccc6336950c05db714f82d10340d51a1
Alex Roan <alex@cyfrin.io>
2024-07-16 13:41:11 +0100

    initial file setup

---
+++ aderyn_core/src/detect/high/state_variable_shadowing.rs
@@ -0,0 +1,87 @@
+use std::collections::BTreeMap;
+use std::error::Error;
+
+use crate::ast::{NodeID, TypeName};
+
+use crate::capture;
+use crate::detect::detector::IssueDetectorNamePool;
+use crate::{
+    context::workspace_context::WorkspaceContext,
+    detect::detector::{IssueDetector, IssueSeverity},
+};
+use eyre::Result;
+
+// HOW TO USE THIS TEMPLATE:
+// 1. Copy this file and rename it to the snake_case version of the issue you are detecting.
+// 2. Rename the StateVariableShadowingDetector struct and impl to your new issue name.
+// 3. Add this file and detector struct to the mod.rs file in the same directory.
+// 4. Implement the detect function to find instances of the issue.
+
+#[derive(Default)]
+pub struct StateVariableShadowingDetector {
+    // Keys are: [0] source file name, [1] line number, [2] character location of node.
+    // Do not add items manually, use `capture!` to add nodes to this BTreeMap.
+    found_instances: BTreeMap<(String, usize, String), NodeID>,
+}
+
+impl IssueDetector for StateVariableShadowingDetector {
+    fn detect(&mut self, context: &WorkspaceContext) -> Result<bool, Box<dyn Error>> {
+        // When you have found an instance of the issue,
+        // use the following macro to add it to `found_instances`:
+        //
+        // capture!(self, context, item);
+
+        Ok(!self.found_instances.is_empty())
+    }
+
+    fn severity(&self) -> IssueSeverity {
+        IssueSeverity::High
+    }
+
+    fn title(&self) -> String {
+        String::from("High Issue Title")
+    }
+
+    fn description(&self) -> String {
+        String::from("Description of the high issue.")
+    }
+
+    fn instances(&self) -> BTreeMap<(String, usize, String), NodeID> {
+        self.found_instances.clone()
+    }
+
+    fn name(&self) -> String {
+        format!("high-issue-template")
+    }
+}
+
+#[cfg(test)]
+mod state_variable_shadowing_detector_tests {
+    use crate::detect::{detector::IssueDetector, high::StateVariableShadowingDetector};
+
+    #[test]
+    fn test_state_variable_shadowing_detector() {
+        let context = crate::detect::test_utils::load_solidity_source_unit(
+            "../tests/contract-playground/src/ArbitraryTransferFrom.sol",
+        );
+
+        let mut detector = StateVariableShadowingDetector::default();
+        let found = detector.detect(&context).unwrap();
+        // assert that the detector found an issue
+        assert!(found);
+        // assert that the detector found the correct number of instances
+        assert_eq!(detector.instances().len(), 1);
+        // assert the severity is high
+        assert_eq!(
+            detector.severity(),
+            crate::detect::detector::IssueSeverity::High
+        );
+        // assert the title is correct
+        assert_eq!(detector.title(), String::from("High Issue Title"));
+        // assert the description is correct
+        assert_eq!(
+            detector.description(),
+            String::from("Description of the high issue.")
+        );
+    }
+}
