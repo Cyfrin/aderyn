4a9a78d23e0fab528505c1f8c114848ee7b2492b
TilakMaddy <tilak.madichetti@gmail.com>
2024-07-21 10:44:39 +0530

    trait fallback

--- aderyn_core/src/context/investigator/traits.rs
+++ aderyn_core/src/context/investigator/traits.rs
@@ -1,4 +1,8 @@
 //! Trackers can implement the following traits to interact with investigators
+//!
+//! NOTE
+//! Upstream and downstream here is relative to [`super::StandardInvestigator::entry_points`]
+//! which is initialized with [`super::StandardInvestigator::new`] function.
 
 use crate::{
     ast::{FunctionDefinition, ModifierDefinition},
@@ -12,37 +16,63 @@ pub trait StandardInvestigatorVisitor {
     /// contain all of the tracking logic in the tracker. Therefore, visit entry point
     /// is essential because the tracker can get to take a look at not just the
     /// downstream functions and modifiers, but also the entry points that have invoked it.
-    fn visit_entry_point(&mut self, node: &ASTNode) -> eyre::Result<()>;
+    fn visit_entry_point(&mut self, node: &ASTNode) -> eyre::Result<()> {
+        self.visit_fallback(node)
+    }
 
     /// Meant to be invoked while traversing [`crate::context::workspace_context::WorkspaceContext::forward_callgraph`]
     fn visit_downstream_function_definition(
         &mut self,
-        _node: &FunctionDefinition,
+        node: &FunctionDefinition,
     ) -> eyre::Result<()> {
-        Ok(())
+        self.visit_fallback(&(node.into()))
     }
 
     /// Meant to be invoked while traversing [`crate::context::workspace_context::WorkspaceContext::reverse_callgraph`]
     fn visit_upstream_function_definition(
         &mut self,
-        _node: &FunctionDefinition,
+        node: &FunctionDefinition,
     ) -> eyre::Result<()> {
-        Ok(())
+        self.visit_fallback(&(node.into()))
     }
 
     /// Meant to be invoked while traversing [`crate::context::workspace_context::WorkspaceContext::forward_callgraph`]
     fn visit_downstream_modifier_definition(
         &mut self,
-        _node: &ModifierDefinition,
+        node: &ModifierDefinition,
     ) -> eyre::Result<()> {
-        Ok(())
+        self.visit_fallback(&(node.into()))
     }
 
     /// Meant to be invoked while traversing [`crate::context::workspace_context::WorkspaceContext::reverse_callgraph`]
     fn visit_upstream_modifier_definition(
         &mut self,
-        _node: &ModifierDefinition,
+        node: &ModifierDefinition,
     ) -> eyre::Result<()> {
+        self.visit_fallback(&(node.into()))
+    }
+
+    /// Read as "upstream's downstream-side-effect" function definition
+    /// These are function definitions that are downstream from the upstream nodes
+    /// but are themselves neither upstream nor downstream to the entry points
+    fn visit_upstream_side_effect_function_definition(
+        &mut self,
+        node: &FunctionDefinition,
+    ) -> eyre::Result<()> {
+        self.visit_fallback(&(node.into()))
+    }
+
+    /// Read as "upstream's downstream-side-effect" modifier definition
+    /// These are modifier definitions that are downstream from the upstream nodes
+    /// but are themselves neither upstream nor downstream to the entry points
+    fn visit_upstream_side_effect_modifier_definition(
+        &mut self,
+        node: &ModifierDefinition,
+    ) -> eyre::Result<()> {
+        self.visit_fallback(&(node.into()))
+    }
+
+    fn visit_fallback(&mut self, _node: &ASTNode) -> eyre::Result<()> {
         Ok(())
     }
 }
