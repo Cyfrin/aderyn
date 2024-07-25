85ff2c041e520ee30b326b484e0cf7ec2c9a84f0
David Drobnak <david.drobnak26@gmail.com>
2024-07-25 16:49:35 +0200

    check for hashing block.number or timestamp

--- aderyn_core/src/detect/high/weak_randomness.rs
+++ aderyn_core/src/detect/high/weak_randomness.rs
@@ -1,12 +1,12 @@
 use std::collections::BTreeMap;
 use std::error::Error;
 
-use crate::ast::NodeID;
+use crate::ast::{Expression, FunctionCall, NodeID};
 
 use crate::capture;
 use crate::detect::detector::IssueDetectorNamePool;
 use crate::{
-    context::workspace_context::WorkspaceContext,
+    context::workspace_context::{ASTNode, WorkspaceContext},
     detect::detector::{IssueDetector, IssueSeverity},
 };
 use eyre::Result;
@@ -20,6 +20,37 @@ pub struct WeakRandomnessDetector {
 
 impl IssueDetector for WeakRandomnessDetector {
     fn detect(&mut self, context: &WorkspaceContext) -> Result<bool, Box<dyn Error>> {
+        let keccaks: Vec<&FunctionCall> = context.function_calls()
+            .into_iter()
+            .filter(|x| matches!(*x.expression, Expression::Identifier(ref id) if id.name == "keccak256"))
+            .collect();
+
+        for keccak in keccaks {
+            // keccak256 must have exactly one argument
+            let arg = keccak.arguments.get(0).unwrap();
+
+            if let Expression::FunctionCall(ref function_call) = *arg {
+                if check_encode(function_call) {
+                    capture!(self, context, keccak);
+                }
+            }
+
+            // check if the variable is defined using encode or encodePacked
+            else if let Expression::Identifier(ref i) = *arg {
+                if let Some(node_id) = i.referenced_declaration {
+                    let decleration = context.get_parent(node_id);
+
+                    if let Some(ASTNode::VariableDeclarationStatement(var)) = decleration {
+                        if let Some(Expression::FunctionCall(function_call)) = &var.initial_value {
+                            if check_encode(function_call) {
+                                capture!(self, context, keccak);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         Ok(!self.found_instances.is_empty())
     }
 
@@ -44,6 +75,28 @@ impl IssueDetector for WeakRandomnessDetector {
     }
 }
 
+// returns whether block.timestamp or block.number is in encode function
+fn check_encode(function_call: &FunctionCall) -> bool {
+    if let Expression::MemberAccess(ref member_access) = *function_call.expression {
+        if member_access.member_name == "encodePacked" || member_access.member_name == "encode" {
+            for argument in &function_call.arguments {
+                if let Expression::MemberAccess(ref member_access) = *argument {
+                    if member_access.member_name == "timestamp" || member_access.member_name == "number" {
+                        if let Expression::Identifier(ref identifier) =
+                            *member_access.expression
+                            {
+                                if identifier.name == "block" {
+                                    return true;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 mod weak_randomness_detector_tests {
     use crate::detect::{detector::IssueDetector, high::weak_randomness::WeakRandomnessDetector};
@@ -59,7 +112,7 @@ mod weak_randomness_detector_tests {
         // assert that the detector found an issue
         assert!(found);
         // assert that the detector found the correct number of instances
-        assert_eq!(detector.instances().len(), 1);
+        assert_eq!(detector.instances().len(), 4);
         // assert the severity is high
         assert_eq!(
             detector.severity(),
--- tests/contract-playground/src/WeakRandomness.sol
+++ tests/contract-playground/src/WeakRandomness.sol
@@ -6,4 +6,35 @@ contract WeakRandomness {
         uint256 randomNumber = uint256(keccak256(abi.encodePacked(msg.sender, block.prevrandao, block.timestamp)));
         return randomNumber;
     }
+
+    function getRandomNumber2() external view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.number)));
+    }
+
+    function getRandomNumber3() external view returns (uint256) {
+        bytes memory someBytes = abi.encode(block.number, msg.sender);
+        return uint256(keccak256(someBytes));
+    }
+
+    function getRandomNumber4() external view returns (uint256) {
+        bytes memory someBytes = abi.encodePacked(block.number, msg.sender);
+        return uint256(keccak256(someBytes));
+    }
+
+    // good
+    function timestamp() external view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function number() external view returns (uint256) {
+        return block.number;
+    }
+
+    function encode() external view returns (bytes memory) {
+        return abi.encode(block.timestamp, block.number);
+    }
+
+    function encodePacked() external view returns (bytes memory) {
+        return abi.encodePacked(block.timestamp, block.number);
+    }
 }
