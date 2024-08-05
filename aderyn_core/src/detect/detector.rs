790fe9c2056e2c1fc85ff6647c5fcc9a4923c9b4
TilakMaddy <tilak.madichetti@gmail.com>
2024-08-05 15:38:48 +0530

    detector

--- aderyn_core/src/detect/detector.rs
+++ aderyn_core/src/detect/detector.rs
@@ -1,3 +1,4 @@
+use incorrect_erc20_interface::IncorrectERC20InterfaceDetector;
 use serde::{Deserialize, Serialize};
 use strum::{Display, EnumCount, EnumIter, EnumString};
 
@@ -77,6 +78,7 @@ pub fn get_all_issue_detectors() -> Vec<Box<dyn IssueDetector>> {
         Box::<TxOriginUsedForAuthDetector>::default(),
         Box::<MsgValueUsedInLoopDetector>::default(),
         Box::<ContractLocksEtherDetector>::default(),
+        Box::<IncorrectERC20InterfaceDetector>::default(),
     ]
 }
 
@@ -150,6 +152,7 @@ pub(crate) enum IssueDetectorNamePool {
     TxOriginUsedForAuth,
     MsgValueInLoop,
     ContractLocksEther,
+    IncorrectERC20Interface,
     // NOTE: `Undecided` will be the default name (for new bots).
     // If it's accepted, a new variant will be added to this enum before normalizing it in aderyn
     Undecided,
@@ -159,6 +162,9 @@ pub fn request_issue_detector_by_name(detector_name: &str) -> Option<Box<dyn Iss
     // Expects a valid detector_name
     let detector_name = IssueDetectorNamePool::from_str(detector_name).ok()?;
     match detector_name {
+        IssueDetectorNamePool::IncorrectERC20Interface => {
+            Some(Box::<IncorrectERC20InterfaceDetector>::default())
+        }
         IssueDetectorNamePool::DelegateCallInLoop => {
             Some(Box::<DelegateCallInLoopDetector>::default())
         }
