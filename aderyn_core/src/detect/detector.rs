4ac8dcc6c87f783644f6f314159f1ac30bb01e24
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-08-06 14:01:44 +0530

    Detector: Return bomb (#645)
    Co-authored-by: Alex Roan <alex@cyfrin.io>

--- aderyn_core/src/detect/detector.rs
+++ aderyn_core/src/detect/detector.rs
@@ -80,6 +80,7 @@ pub fn get_all_issue_detectors() -> Vec<Box<dyn IssueDetector>> {
         Box::<TxOriginUsedForAuthDetector>::default(),
         Box::<MsgValueUsedInLoopDetector>::default(),
         Box::<ContractLocksEtherDetector>::default(),
+        Box::<ReturnBombDetector>::default(),
     ]
 }
 
@@ -156,6 +157,7 @@ pub(crate) enum IssueDetectorNamePool {
     TxOriginUsedForAuth,
     MsgValueInLoop,
     ContractLocksEther,
+    ReturnBomb,
     // NOTE: `Undecided` will be the default name (for new bots).
     // If it's accepted, a new variant will be added to this enum before normalizing it in aderyn
     Undecided,
@@ -165,6 +167,7 @@ pub fn request_issue_detector_by_name(detector_name: &str) -> Option<Box<dyn Iss
     // Expects a valid detector_name
     let detector_name = IssueDetectorNamePool::from_str(detector_name).ok()?;
     match detector_name {
+        IssueDetectorNamePool::ReturnBomb => Some(Box::<ReturnBombDetector>::default()),
         IssueDetectorNamePool::UnusedStateVariable => {
             Some(Box::<UnusedStateVariablesDetector>::default())
         }
