c8c7ceae41929df87fe448194e6ff34c1892372e
TilakMaddy <tilak.madichetti@gmail.com>
2024-08-12 13:15:19 +0530

    detector now triggers for only <0.5.0

--- aderyn_core/src/detect/high/const_func_change_state.rs
+++ aderyn_core/src/detect/high/const_func_change_state.rs
@@ -5,6 +5,9 @@ use crate::ast::{NodeID, StateMutability};
 
 use crate::capture;
 use crate::context::browser::ApproximateStorageChangeFinder;
+use crate::context::investigator::{
+    StandardInvestigationStyle, StandardInvestigator, StandardInvestigatorVisitor,
+};
 use crate::detect::detector::IssueDetectorNamePool;
 use crate::detect::helpers;
 use crate::{
@@ -12,12 +15,6 @@ use crate::{
     detect::detector::{IssueDetector, IssueSeverity},
 };
 use eyre::Result;
-
-// HOW TO USE THIS TEMPLATE:
-// 1. Copy this file and rename it to the snake_case version of the issue you are detecting.
-// 2. Rename the ConstantFunctionChangingStateDetector struct and impl to your new issue name.
-// 3. Add this file and detector struct to the mod.rs file in the same directory.
-// 4. Implement the detect function to find instances of the issue.
 
 #[derive(Default)]
 pub struct ConstantFunctionChangingStateDetector {
@@ -33,9 +30,23 @@ impl IssueDetector for ConstantFunctionChangingStateDetector {
             if func.state_mutability() != &StateMutability::View {
                 continue;
             }
-            // Check for state variable changes
-            let finder = ApproximateStorageChangeFinder::from(context, func);
-            if finder.state_variables_have_been_manipulated() {
+            // Check if this func is compilable for solc < 0.5.0. If not, move on to the next
+            if !func.compiles_for_solc_below_0_5_0(context) {
+                continue;
+            }
+            // Now, investigate the function to see if there is scope for any state variable changes
+            let mut tracker = StateVariableChangeTracker {
+                state_var_has_changed: false,
+                context,
+            };
+            let investigator = StandardInvestigator::new(
+                context,
+                &[&(func.into())],
+                StandardInvestigationStyle::Downstream,
+            )?;
+            investigator.investigate(context, &mut tracker)?;
+
+            if tracker.state_var_has_changed {
                 capture!(self, context, func);
             }
         }
@@ -52,7 +63,7 @@ impl IssueDetector for ConstantFunctionChangingStateDetector {
     }
 
     fn description(&self) -> String {
-        String::from("Function is declared as a constant function but it changes state. Ensure that the attributes of contract compiled prior to 0.5 are correct.")
+        String::from("Function is declared constant/view but it changes state. Ensure that the attributes of contract compiled prior to 0.5 are correct.")
     }
 
     fn instances(&self) -> BTreeMap<(String, usize, String), NodeID> {
@@ -61,6 +72,72 @@ impl IssueDetector for ConstantFunctionChangingStateDetector {
 
     fn name(&self) -> String {
         IssueDetectorNamePool::ConstantFunctionChangingState.to_string()
+    }
+}
+
+struct StateVariableChangeTracker<'a> {
+    state_var_has_changed: bool,
+    context: &'a WorkspaceContext,
+}
+
+impl<'a> StandardInvestigatorVisitor for StateVariableChangeTracker<'a> {
+    fn visit_any(&mut self, node: &crate::ast::ASTNode) -> eyre::Result<()> {
+        if self.state_var_has_changed {
+            return Ok(());
+        }
+        // Check for state variable changes
+        let finder = ApproximateStorageChangeFinder::from(self.context, node);
+        if finder.state_variables_have_been_manipulated() {
+            self.state_var_has_changed = true;
+        }
+        Ok(())
+    }
+}
+
+mod func_compilation_solc_pragma_helper {
+    use std::str::FromStr;
+
+    use semver::{Version, VersionReq};
+
+    use crate::{
+        ast::{FunctionDefinition, NodeType},
+        context::{
+            browser::{ExtractPragmaDirectives, GetClosestAncestorOfTypeX},
+            workspace_context::WorkspaceContext,
+        },
+        detect::helpers,
+    };
+
+    impl FunctionDefinition {
+        pub fn compiles_for_solc_below_0_5_0(&self, context: &WorkspaceContext) -> bool {
+            if let Some(source_unit) = self.closest_ancestor_of_type(context, NodeType::SourceUnit)
+            {
+                let pragma_directives = ExtractPragmaDirectives::from(source_unit).extracted;
+
+                if let Some(pragma_directive) = pragma_directives.first() {
+                    if let Ok(pragma_semver) = helpers::pragma_directive_to_semver(pragma_directive)
+                    {
+                        if version_req_allows_below_0_5_0(&pragma_semver) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            false
+        }
+    }
+
+    fn version_req_allows_below_0_5_0(version_req: &VersionReq) -> bool {
+        // If it matches any 0.4.0 to 0.4.26, return true
+        for i in 0..=26 {
+            let version = Version::from_str(&format!("0.4.{}", i)).unwrap();
+            if version_req.matches(&version) {
+                return true;
+            }
+        }
+
+        // Else, return false
+        false
     }
 }
 
