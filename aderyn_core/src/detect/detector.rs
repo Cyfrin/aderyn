87e23f007f2d50bf928e44b16e061c85a58d8987
TilakMaddy <tilak.madichetti@gmail.com>
2024-08-23 13:55:01 +0530

    detector

--- aderyn_core/src/detect/detector.rs
+++ aderyn_core/src/detect/detector.rs
@@ -93,6 +93,7 @@ pub fn get_all_issue_detectors() -> Vec<Box<dyn IssueDetector>> {
         Box::<ConstantFunctionChangingStateDetector>::default(),
         Box::<BuiltinSymbolShadowDetector>::default(),
         Box::<FunctionSelectorCollisionDetector>::default(),
+        Box::<FucntionPointerInConstructorDetector>::default(),
     ]
 }
 
@@ -104,6 +105,7 @@ pub fn get_all_detectors_names() -> Vec<String> {
 #[derive(Debug, PartialEq, EnumString, Display)]
 #[strum(serialize_all = "kebab-case")]
 pub(crate) enum IssueDetectorNamePool {
+    FunctionPointerInConstructor,
     FunctionSelectorCollision,
     CacheArrayLength,
     AssertStateChange,
@@ -189,6 +191,9 @@ pub fn request_issue_detector_by_name(detector_name: &str) -> Option<Box<dyn Iss
     // Expects a valid detector_name
     let detector_name = IssueDetectorNamePool::from_str(detector_name).ok()?;
     match detector_name {
+        IssueDetectorNamePool::FunctionPointerInConstructor => {
+            Some(Box::<FucntionPointerInConstructorDetector>::default())
+        }
         IssueDetectorNamePool::FunctionSelectorCollision => {
             Some(Box::<FunctionSelectorCollisionDetector>::default())
         }
