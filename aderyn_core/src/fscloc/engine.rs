a6a1dbea2d3f568d1121b10612b1e3c059f2dc93
Tilak Madichetti <tilak.madichetti@gmail.com>
2024-08-28 18:33:33 +0530

    Feature: Set default source to `contracts/` for hardhat projects (#705)

--- aderyn_core/src/fscloc/engine.rs
+++ aderyn_core/src/fscloc/engine.rs
@@ -10,27 +10,45 @@ pub fn count_lines_of_code_and_collect_line_numbers_to_ignore(
     src_filepaths: &[String],
     skip_cloc: bool,
 ) -> Mutex<HashMap<String, Stats>> {
-    let walker = WalkBuilder::new(src);
     let (tx, rx) = crossbeam_channel::unbounded();
-    walker.build_parallel().run(|| {
-        let tx = tx.clone();
-        Box::new(move |res| {
-            if let Ok(target) = res {
-                let target_path = target.path().to_str();
-                if target.file_type().unwrap().is_file() {
-                    // dbg!(target_path.unwrap());
-                    if src_filepaths
-                        .iter()
-                        .any(|fp| target_path.map_or(false, |path| path.contains(fp)))
-                    {
+
+    let form_path = |path: &String| {
+        // Soldiity compiler shenanigans ??
+        // In the line  `==== ??? ====` of the output, we're supposed to see the filename
+        // But sometimes solc puts filenames with path containing two forward slashes
+        // Example `contracts/templegold//AuctionBase.sol` in there
+        // Although there is a separate entry for `contracts/templegold/AuctionBase.sol`.
+        // We want to omit reading the former
+        if path.contains("//") {
+            None
+        } else {
+            Some(Path::new(&src).join(path))
+        }
+    };
+
+    let src_filepaths_as_paths = src_filepaths.iter().flat_map(form_path).collect::<Vec<_>>();
+
+    if !src_filepaths_as_paths.is_empty() {
+        // Only add the paths to WalkBuilder that we want to do analysis on.
+        let mut walker = WalkBuilder::new(src_filepaths_as_paths[0].clone());
+
+        for item in src_filepaths_as_paths.iter().skip(1) {
+            walker.add(item);
+        }
+
+        walker.build_parallel().run(|| {
+            let tx = tx.clone();
+            Box::new(move |res| {
+                if let Ok(target) = res {
+                    if target.file_type().unwrap().is_file() {
                         let send = target.to_owned();
                         tx.send(send).unwrap();
                     }
                 }
-            }
-            Continue
-        })
-    });
+                Continue
+            })
+        });
+    }
 
     drop(tx); // without this, the program would not terminate .. becoz receiver would
               // think that the `tx` is still waiting to send something.. but we're done
