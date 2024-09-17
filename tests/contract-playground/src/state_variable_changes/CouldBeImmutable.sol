1e4cd679c65f585e53ccd7c91fcd95d2102ba12b
TilakMaddy <tilak.madichetti@gmail.com>
2024-09-17 23:52:13 +0530

    mc file

---
+++ tests/contract-playground/src/state_variable_changes/CouldBeImmutable.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+// aderyn-ignore-next-line
+contract StateVarImmutable {
+    uint256 public immutableValue; // It can be marked immutable
+    uint256 public seeminglyImmutableValue; // It cannot be marked immutable
+    uint256 public variableValue;
+
+    // aderyn-ignore-next-line(state-variable-could-be-declared-constant)
+    address public h = address(3); // This is a candidate for constant
+
+    uint256 private immutable x;
+
+    constructor() {
+        x = 103; // aderyn-ignore
+        immutableValue = 50; // aderyn-ignore
+        immutableValue *= 3; // aderyn-ignore
+        variableValue = 130; // aderyn-ignore
+        changeSeeminglyImmutableValue();
+    }
+
+    // aderyn-ignore-next-line
+    function changeSeeminglyImmutableValue() internal {
+        // When you try to initialize an immutable value in an internal function that is exclsively
+        // called by the constructor, yuo get the following error:
+
+        // ERROR:
+        // Cannot write to immutable here: Immutable variables can only be
+        // initialized inline or assigned directly in the constructor.
+
+        // As a result even though `seeminglyImmutableValue` is init only in the constructor, because it's
+        // value is not directly assigned in the constructor definition, we must not consider that as a potentially
+        // suitable immutable variable
+
+        seeminglyImmutableValue *= 2; // aderyn-ignore
+    }
+
+    // aderyn-ignore-next-line(state-variable-changes-without-events)
+    function changeItNow() external {
+        variableValue = uint256(uint160(h));
+    }
+}
