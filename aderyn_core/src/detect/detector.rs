18ed5c1e3eaba013e8323458fda4f39dae4654cc
TilakMaddy <tilak.madichetti@gmail.com>
2024-10-07 15:29:25 +0530

    re-entrancy low detector

--- aderyn_core/src/detect/detector.rs
+++ aderyn_core/src/detect/detector.rs
@@ -106,6 +106,7 @@ pub fn get_all_issue_detectors() -> Vec<Box<dyn IssueDetector>> {
         Box::<StateVariableCouldBeImmutableDetector>::default(),
         Box::<MultiplePlaceholdersDetector>::default(),
         Box::<StateChangeAfterExternalCallDetector>::default(),
+        Box::<EmitAfterExternalCallDetector>::default(),
     ]
 }
 
@@ -117,6 +118,7 @@ pub fn get_all_detectors_names() -> Vec<String> {
 #[derive(Debug, PartialEq, EnumString, Display)]
 #[strum(serialize_all = "kebab-case")]
 pub(crate) enum IssueDetectorNamePool {
+    EmitAfterExternalCall,
     StateChangeAfterExternalCall,
     StateVariableCouldBeDeclaredImmutable,
     MultiplePlaceholders,
@@ -215,6 +217,9 @@ pub fn request_issue_detector_by_name(detector_name: &str) -> Option<Box<dyn Iss
     // Expects a valid detector_name
     let detector_name = IssueDetectorNamePool::from_str(detector_name).ok()?;
     match detector_name {
+        IssueDetectorNamePool::EmitAfterExternalCall => {
+            Some(Box::<EmitAfterExternalCallDetector>::default())
+        }
         IssueDetectorNamePool::StateChangeAfterExternalCall => {
             Some(Box::<StateChangeAfterExternalCallDetector>::default())
         }
