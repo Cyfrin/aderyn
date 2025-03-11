65b11eb5ccc3558337ae614d0121097886f776e1
Alex Roan <alex@cyfrin.io>
2025-03-11 20:25:02 +0000

    Fixed ignore statement in test

--- tests/contract-playground/src/IgnoreEverything.sol
+++ tests/contract-playground/src/IgnoreEverything.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 contract IgnoreEverything {
     // This will be not reported by either of the detectors
-    // aderyn-ignore-next-line(useless-public-function,empty-block)
+    // aderyn-ignore-next-line(unused-public-function,empty-block)
     function f2() public {}
 
     // This will never be reported
