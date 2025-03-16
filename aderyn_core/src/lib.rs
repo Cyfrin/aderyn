22c575bf114ba101349c3d81d005c911c65ca125
TilakMaddy <tilak.madichetti@gmail.com>
2025-03-16 15:46:38 +0530

    Revert "tech-debt: Free aderyn_core from cyfrin_foundry_config (#840)"
    This reverts commit c63359ee8407ea767fa00bdf5e5e21148dff28aa.

--- aderyn_core/src/lib.rs
+++ aderyn_core/src/lib.rs
@@ -7,6 +7,7 @@ pub mod report;
 pub mod visitor;
 
 use audit::auditor::{get_auditor_detectors, AuditorPrinter, BasicAuditorPrinter};
+use cyfrin_foundry_compilers::utils::canonicalize;
 use detect::detector::IssueDetector;
 use eyre::Result;
 use fscloc::cloc::When;
@@ -25,13 +26,6 @@ use std::{
 use crate::{context::workspace_context::WorkspaceContext, detect::detector::IssueSeverity};
 
 use crate::report::{printer::ReportPrinter, reporter::Report, Issue};
-
-pub fn canonicalize(
-    path: impl AsRef<Path>,
-) -> Result<PathBuf, Box<dyn std::error::Error + 'static>> {
-    let path = path.as_ref();
-    Ok(dunce::canonicalize(path)?)
-}
 
 #[allow(clippy::too_many_arguments)]
 pub fn run<T>(
