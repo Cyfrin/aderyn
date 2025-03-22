21acca46ce1d2b0d30c6c786f9fa798a78d92d45
Tilak Madichetti <tilak.madichetti@gmail.com>
2025-03-22 18:03:16 +0530

    Internal tools: cut-release (#853)

---
+++ tools/xtask/src/fixpr.rs
@@ -0,0 +1,46 @@
+use xshell::{Shell, cmd};
+
+pub fn fixpr() -> anyhow::Result<()> {
+    let sh = Shell::new()?;
+    sh.change_dir(env!("CARGO_MANIFEST_DIR"));
+    sh.change_dir("../../");
+
+    // Run cargo build (sanity test)
+    let cmd = cmd!(sh, "cargo build");
+    cmd.run()?;
+
+    // Fix format
+    let cmd = cmd!(sh, "cargo +nightly fmt --all");
+    cmd.run()?;
+
+    // Check fixed format
+    let cmd = cmd!(sh, "cargo +nightly fmt --all --check");
+    cmd.run()?;
+
+    // Fix clippy
+    let cmd = cmd!(
+        sh,
+        "cargo clippy --quiet --workspace --all-targets --all-features --allow-dirty --fix"
+    );
+    cmd.run()?;
+
+    // Check clippy
+    let cmd = cmd!(sh, "cargo clippy").arg("--").arg("-D").arg("warnings");
+    cmd.run()?;
+
+    // Create reportgen
+    let cmd = cmd!(sh, "chmod +x ./cli/reportgen.sh");
+    cmd.run()?;
+    let cmd = cmd!(sh, "./cli/reportgen.sh");
+    cmd.run()?;
+
+    // Push changes
+    let cmd = cmd!(sh, "git add .");
+    cmd.run()?;
+    let cmd = cmd!(sh, "git commit -am").arg("chore: cargo fixpr");
+    cmd.run()?;
+    let cmd = cmd!(sh, "git push");
+    cmd.run()?;
+
+    Ok(())
+}
--- tools/xtask/src/flags.rs
+++ tools/xtask/src/flags.rs
@@ -2,7 +2,16 @@ xflags::xflags! {
     src "./src/flags.rs"
 
     cmd xtask {
-        cmd r {
+        cmd cut-release {
+            /// Cut a patch release
+            optional -p, --patch
+
+            /// Cut a minor release
+            optional -m, --minor
+        }
+        cmd fixpr {
+        }
+        cmd reportgen {
             optional -a, --all
 
             /// Sablier
@@ -51,11 +60,22 @@ pub struct Xtask {
 
 #[derive(Debug)]
 pub enum XtaskCmd {
-    R(R),
+    CutRelease(CutRelease),
+    Fixpr(Fixpr),
+    Reportgen(Reportgen),
 }
 
 #[derive(Debug)]
-pub struct R {
+pub struct CutRelease {
+    pub patch: bool,
+    pub minor: bool,
+}
+
+#[derive(Debug)]
+pub struct Fixpr;
+
+#[derive(Debug)]
+pub struct Reportgen {
     pub all: bool,
     pub sablier: bool,
     pub tg: bool,
