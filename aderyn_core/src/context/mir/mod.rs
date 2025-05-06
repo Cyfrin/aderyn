ea6ce992c35749235094f79dd4baf0836261804c
TilakMaddy <tilak.madichetti@gmail.com>
2025-05-06 11:15:14 +0530

    w

--- aderyn_core/src/context/mir/mod.rs
+++ aderyn_core/src/context/mir/mod.rs
@@ -1,5 +1,9 @@
 //! Mir - Middle Intermediate Representation
+//!
+//! Goal
+//! * Preprocess callgraph for each deployable contract
 
 pub mod router;
+pub mod utils;
 
 pub struct Mir {}
---
+++ aderyn_core/src/context/mir/utils.rs
@@ -0,0 +1,7 @@
+use crate::context::workspace_context::WorkspaceContext;
+
+use super::Mir;
+
+impl Mir {
+    pub fn build(context: &WorkspaceContext) {}
+}
