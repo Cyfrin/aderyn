c6d773533624586f98e7e212b0e1df894896c62c
TilakMaddy <tilak.madichetti@gmail.com>
2025-05-11 23:04:17 +0530

    chore: cargo blesspr

--- aderyn_core/src/ast/impls/ctx/workspace.rs
+++ aderyn_core/src/ast/impls/ctx/workspace.rs
@@ -1,6 +1,12 @@
 use rayon::iter::{IntoParallelIterator, ParallelBridge};
 
-use crate::{ast::ContractDefinition, context::workspace::WorkspaceContext};
+use crate::{
+    ast::{ContractDefinition, FunctionDefinition},
+    context::{
+        graph::{CallGraphConsumer, CallGraphDirection},
+        workspace::WorkspaceContext,
+    },
+};
 
 impl WorkspaceContext {
     #[inline]
@@ -13,5 +19,30 @@ impl WorkspaceContext {
         &self,
     ) -> impl IntoParallelIterator<Item = &ContractDefinition> {
         self.contract_definitions().into_iter().filter(|c| c.is_deployable_contract()).par_bridge()
+    }
+
+    pub fn entrypoints_of_deployable_contracts(&self) -> Vec<&FunctionDefinition> {
+        let mut entrypoints = vec![];
+        for contract in self.deployable_contracts() {
+            if let Some(entrypoint_funcs) = contract.entrypoint_functions(self) {
+                entrypoints.extend(entrypoint_funcs);
+            }
+        }
+        entrypoints
+    }
+
+    pub fn entrypoints_with_callgraphs(
+        &self,
+    ) -> Vec<(&FunctionDefinition, Vec<CallGraphConsumer>)> {
+        let mut result = vec![];
+        for func in self.entrypoints_of_deployable_contracts() {
+            let Ok(callgraphs) =
+                CallGraphConsumer::get(self, &[&(func.into())], CallGraphDirection::Inward)
+            else {
+                continue;
+            };
+            result.push((func, callgraphs));
+        }
+        result
     }
 }
