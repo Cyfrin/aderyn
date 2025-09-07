da6496d8bda5e2968b9c15b8c323781ecbc161e7
Tilak Madichetti <tilak.madichetti@gmail.com>
2025-09-07 12:06:55 +0530

    fix: remove node modules  (#980)

--- node_modules/@cyfrin/aderyn/node_modules/axios-proxy-builder/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configureProxy = void 0;
-var proxy_builder_1 = require("./proxy-builder");
-Object.defineProperty(exports, "configureProxy", { enumerable: true, get: function () { return proxy_builder_1.configureProxy; } });
--- node_modules/@cyfrin/aderyn/node_modules/axios-proxy-builder/dist/test-fixtures.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.http_env = exports.https_env = exports.bad_proto_request = exports.http_request = exports.https_request = void 0;
-exports.https_request = new URL("https://test.com:8000");
-exports.http_request = new URL("http://test.com:8000");
-exports.bad_proto_request = new URL("rss://test.com:8000");
-exports.https_env = "https://testproxy.com:8000";
-exports.http_env = "http://testproxy.com:8000";
