c1635a7901144f8f314162fcb7d702077df4055f
Tilak Madichetti <tilak.madichetti@gmail.com>
2025-09-26 21:46:15 +0530

    chore: allow unstable feature map_or_default (#1005)

--- aderyn_driver/src/lib.rs
+++ aderyn_driver/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(result_option_map_or_default)]
+
 mod display;
 mod interface;
 mod mcp;
@@ -10,20 +12,3 @@ pub mod process;
 
 pub use aderyn_core::{ast as core_ast, detect as detection_modules, detect::detector};
 pub use mcp::SingletonMcpServer;
-
-pub trait MapOrDefault<T> {
-    fn map_or_default<U, F>(self, f: F) -> U
-    where
-        U: Default,
-        F: FnOnce(T) -> U;
-}
-
-impl<T> MapOrDefault<T> for Option<T> {
-    fn map_or_default<U, F>(self, f: F) -> U
-    where
-        U: Default,
-        F: FnOnce(T) -> U,
-    {
-        self.map(f).unwrap_or_default()
-    }
-}
