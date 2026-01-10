041356b71071dcf0d291c2dd9263489994bd6616
TilakMaddy <tilak.madichetti@gmail.com>
2026-01-10 15:28:19 +0530

    chore: more changes

--- aderyn/src/main.rs
+++ aderyn/src/main.rs
@@ -17,23 +17,20 @@ use std::path::PathBuf;
     author,
     version,
     about = indoc!{
-        r#"Aderyn - Rust based Solidity Static Analyzer
+        r#"Aderyn - Rust based Solidity Static Analyzer (~45k+ total downloads)
 
         CLI Quickstart:
             cd my-solidity-project/
             aderyn
 
-        Official VS Code Extension: (1800+ downloads)
+        VS Code Extension: (1800+ downloads)
             https://marketplace.visualstudio.com/items?itemName=Cyfrin.aderyn
 
         Github Action CI Assistant:
             https://github.com/marketplace/actions/aderyn-ci-assistant
 
         Tip: Run `aderyn init` in the workspace root to create a config file for customizing the scan
-
-        Help Aderyn stay open source by giving us a star on Github - https://github.com/cyfrin/aderyn
-
-        Stats: ~45k total downloads
+        Help Aderyn stay open source. Give a star to https://github.com/cyfrin/aderyn
 
     "#},
     group(ArgGroup::new("stdout_dependent").requires("stdout")),
